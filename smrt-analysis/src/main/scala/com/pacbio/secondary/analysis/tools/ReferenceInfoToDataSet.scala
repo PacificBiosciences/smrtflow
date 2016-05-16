bb30b5f0ff467373e2a4d748a60a588579757e96
Jayson Falkner <jfalkner@pacificbiosciences.com>
2016-05-16 16:03:53 -0400

    ReferenceInfoDataSet update

--- smrt-analysis/src/main/scala/com/pacbio/secondary/analysis/tools/ReferenceInfoToDataSet.scala
+++ smrt-analysis/src/main/scala/com/pacbio/secondary/analysis/tools/ReferenceInfoToDataSet.scala
@@ -2,21 +2,21 @@ package com.pacbio.secondary.analysis.tools
 
 import java.nio.file.Paths
 
+import com.pacbio.logging.{LoggerConfig, LoggerOptions}
 import com.pacbio.secondary.analysis.converters.ReferenceInfoConverter._
-import com.pacbio.secondary.analysis.legacy.{ReferenceInfoUtils, ReferenceEntry}
+import com.pacbio.secondary.analysis.legacy.{ReferenceEntry, ReferenceInfoUtils}
 import org.joda.time.{DateTime => JodaDateTime}
 import scopt.OptionParser
 
 
 case class ReferenceConverterConfig(referenceInfoXMLPath: String,
-                                    datasetXMLPath: String,
-                                    debug: Boolean = false) extends ToolConfig
+                                    datasetXMLPath: String) extends LoggerConfig
 
 object ReferenceInfoToDataSetTool extends CommandLineToolRunner[ReferenceConverterConfig] {
 
   val toolId = "pbscala.tools.reference_info_to_ds"
   val VERSION = "0.3.0"
-  val defaults = ReferenceConverterConfig("", "", debug = false)
+  val defaults = ReferenceConverterConfig("", "")
 
   val parser = new OptionParser[ReferenceConverterConfig]("reference-to-dataset") {
     head("Reference To Reference Dataset XML ", VERSION)
@@ -30,10 +30,8 @@ object ReferenceInfoToDataSetTool extends CommandLineToolRunner[ReferenceConvert
       c.copy(datasetXMLPath = x)
     } text "Path to output Reference Dataset XML"
 
-    opt[Unit]('d', "debug") action { (x, c) =>
-      c.copy(debug = true)
-    } text "Emit logging to stdout."
-
+    // add the shared `--debug` and logging options
+    LoggerOptions.add(this.asInstanceOf[OptionParser[LoggerConfig]])
   }
 
   /**
