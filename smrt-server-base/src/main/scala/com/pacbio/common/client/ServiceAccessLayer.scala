9ccca17451e4367ee225befc7e1e7c08fd880366
natechols <nathaniel.echols@gmail.com>
2016-05-19 20:18:16 -0700

    minimalist service access layer in smrt-server-base, with get-smrt-server-status (same as in smrt-server-tools, minus additional endpoints)

---
+++ smrt-server-base/src/main/scala/com/pacbio/common/client/ServiceAccessLayer.scala
@@ -0,0 +1,79 @@
+package com.pacbio.common.client
+
+import com.pacbio.common.models.{PacBioJsonProtocol, ServiceStatus}
+
+import akka.actor.ActorSystem
+import spray.client.pipelining._
+import scala.concurrent.duration._
+//import spray.http.StatusCode._
+import spray.http._
+import spray.httpx.SprayJsonSupport
+import spray.json.DefaultJsonProtocol
+
+import scala.reflect.Manifest._
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success, Try}
+import scala.xml.XML
+
+import java.net.URL
+import java.util.UUID
+
+
+class ServiceAccessLayer(val baseUrl: URL)(implicit actorSystem: ActorSystem) {
+
+  import PacBioJsonProtocol._
+  import SprayJsonSupport._
+
+  implicit val executionContext = actorSystem.dispatcher
+
+  private def toUrl(segment: String): String =
+    new URL(baseUrl.getProtocol, baseUrl.getHost, baseUrl.getPort, segment).toString
+  private def toUiRootUrl(port: Int): String =
+    new URL(baseUrl.getProtocol, baseUrl.getHost, port, "/").toString
+
+  // Pipelines and serialization
+  def respPipeline: HttpRequest => Future[HttpResponse] = sendReceive
+  def rawJsonPipeline: HttpRequest => Future[String] = sendReceive ~> unmarshal[String]
+  def serviceStatusPipeline: HttpRequest => Future[ServiceStatus] = sendReceive ~> unmarshal[ServiceStatus]
+
+  val statusUrl = toUrl("/status")
+
+  def getStatus: Future[ServiceStatus] = serviceStatusPipeline {
+    Get(statusUrl)
+  }
+
+  def getEndpoint(endpointUrl: String): Future[HttpResponse] = respPipeline {
+    Get(endpointUrl)
+  }
+
+  def getServiceEndpoint(endpointPath: String): Future[HttpResponse] = respPipeline {
+    Get(toUrl(endpointPath))
+  }
+
+  def checkEndpoint(endpointUrl: String): Int = {
+    Try {
+      Await.result(getEndpoint(endpointUrl), 20 seconds)
+    } match {
+      // FIXME need to make this more generic
+      case Success(x) => {
+        x.status match {
+          case StatusCodes.Success(_) =>
+            println(s"found endpoint ${endpointUrl}")
+            0
+          case _ =>
+            println(s"error retrieving ${endpointUrl}: ${x.status}")
+            1
+        }
+      }
+      case Failure(err) => {
+        println(s"failed to retrieve endpoint ${endpointUrl}")
+        println(s"${err}")
+        1
+      }
+    }
+  }
+
+  def checkServiceEndpoint(endpointPath: String): Int = checkEndpoint(toUrl(endpointPath))
+
+  def checkUiEndpoint(uiPort: Int): Int = checkEndpoint(toUiRootUrl(uiPort))
+}
