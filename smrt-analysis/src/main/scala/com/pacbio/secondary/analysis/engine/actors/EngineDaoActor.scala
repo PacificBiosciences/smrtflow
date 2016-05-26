cd749e8de57709c822f25ecfaaf8b84d413d63f8
Steve McClellan <smcclellan@pacificbiosciences.com>
2016-05-26 15:54:25 -0700

    Migrate all db interactions to slick 3.1 model

--- smrt-analysis/src/main/scala/com/pacbio/secondary/analysis/engine/actors/EngineDaoActor.scala
+++ smrt-analysis/src/main/scala/com/pacbio/secondary/analysis/engine/actors/EngineDaoActor.scala
@@ -1,21 +1,18 @@
 package com.pacbio.secondary.analysis.engine.actors
 
-import java.nio.file.Paths
-import java.util.UUID
-
-import akka.actor.{Props, ActorRef, ActorLogging, Actor}
-import com.pacbio.secondary.analysis.datasets.DataSetMetaTypes
-import com.pacbio.secondary.analysis.engine.EngineDao.JobEngineDataStore
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.pattern.pipe
 import com.pacbio.secondary.analysis.engine.CommonMessages
-import com.pacbio.secondary.analysis.jobs.JobModels.{DataStoreJobFile, RunnableJob, PacBioDataStore, DataStoreFile}
+import com.pacbio.secondary.analysis.engine.EngineDao.JobEngineDataStore
 import com.pacbio.secondary.analysis.jobs.AnalysisJobStates
+import com.pacbio.secondary.analysis.jobs.JobModels.{DataStoreFile, DataStoreJobFile, PacBioDataStore, RunnableJob}
 
-import scala.collection.mutable
+import scala.concurrent.ExecutionContext.Implicits._
+import scala.concurrent.Future
 
 /**
  * Actor to talk to the persistence layer.
  *
- * @param dao
  * @param listeners Actors that will be notified when jobs are complete or updated state
  */
 class EngineDaoActor(dao: JobEngineDataStore, listeners: Seq[ActorRef]) extends Actor with ActorLogging {
@@ -34,54 +31,56 @@ class EngineDaoActor(dao: JobEngineDataStore, listeners: Seq[ActorRef]) extends 
       sender ! dao.getJobs(limit)
 
     case GetSystemJobSummary =>
-      val rs = dao.getJobs(1000)
-      val states = rs.map(_.state).toSet
-      // Summary of job states by type
-      val results = states.toList.map(x => (x, rs.count(e => e.state == x)))
-      log.debug(s"Results $results")
-      val dsJobFiles = dao.getDataStoreFiles
-      log.debug(s"Number of DataStore files ${dsJobFiles.size}")
-      dsJobFiles.foreach { x => log.debug(s"DS File $x")}
-      rs.foreach { x => log.debug(s"Job result id ${x.id} -> ${x.uuid} ${x.state} ${x.jobTypeId}") }
+      dao.getJobs(1000).map { rs =>
+        val states = rs.map(_.state).toSet
+        // Summary of job states by type
+        val results = states.toList.map(x => (x, rs.count(e => e.state == x)))
+        log.debug(s"Results $results")
+        dao.getDataStoreFiles.map { dsJobFiles =>
+          log.debug(s"Number of DataStore files ${dsJobFiles.size}")
+          dsJobFiles.foreach { x => log.debug(s"DS File $x") }
+          rs.foreach { x => log.debug(s"Job result id ${x.id} -> ${x.uuid} ${x.state} ${x.jobTypeId}") }
+        }
+      }
 
-    case GetJobStatusByUUID(uuid) =>
-      sender ! dao.getJobByUUID(uuid)
+    case GetJobStatusByUUID(uuid) => dao.getJobByUUID(uuid) pipeTo sender
 
-    case AddNewJob(job) =>
-      dao.addRunnableJob(RunnableJob(job, AnalysisJobStates.CREATED))
-      sender ! SuccessMessage(s"Successfully added Job ${job.uuid}")
+    case AddNewJob(job) => dao.addRunnableJob(RunnableJob(job, AnalysisJobStates.CREATED))
+      .map(_ => SuccessMessage(s"Successfully added Job ${job.uuid}")) pipeTo sender
 
-    case HasNextRunnableJobWithId =>
-      sender ! dao.getNextRunnableJobWithId
+    case HasNextRunnableJobWithId => dao.getNextRunnableJobWithId pipeTo sender
 
     case UpdateJobStatus(uuid, state) =>
-      dao.updateJobStateByUUID(uuid, state)
-      listeners.foreach(x => x ! UpdateJobStatus(uuid, state))
-      sender ! SuccessMessage(s"Successfully updated state of job ${uuid.toString} to ${state.toString}")
+      dao.updateJobStateByUUID(uuid, state).map { _ =>
+        listeners.foreach(x => x ! UpdateJobStatus(uuid, state))
+        SuccessMessage(s"Successfully updated state of job ${uuid.toString} to ${state.toString}")
+      } pipeTo sender
 
     case ImportDataStoreFile(dataStoreFile, jobUUID) =>
         log.debug(s"importing datastore file $dataStoreFile for job ${jobUUID.toString}")
-        sender ! dao.addDataStoreFile(DataStoreJobFile(jobUUID, dataStoreFile))
+        dao.addDataStoreFile(DataStoreJobFile(jobUUID, dataStoreFile)) pipeTo sender
 
     case PacBioImportDataSet(x, jobUUID) =>
       x match {
         case ds: DataStoreFile =>
           log.info(s"importing dataset from $ds")
-          dao.addDataStoreFile(DataStoreJobFile(jobUUID, ds))
-          sender ! Right(SuccessMessage(s"Successfully imported files from $x"))
+          dao.addDataStoreFile(DataStoreJobFile(jobUUID, ds)) pipeTo sender
 
         case ds: PacBioDataStore =>
           log.info(s"loading files from datastore $ds")
-          val results = ds.files.map { dsFile => dao.addDataStoreFile(DataStoreJobFile(jobUUID, dsFile)) }
-          val failures = results.map(x => x.left.toOption).flatMap(a => a)
-          if (failures.nonEmpty) {
-            Left(FailedMessage(s"Failed to import datastore $failures"))
-          } else {
-            sender ! Right(SuccessMessage(s"Successfully imported files from $x"))
-          }
+          val results = Future.sequence(ds.files.map { f => dao.addDataStoreFile(DataStoreJobFile(jobUUID, f)) })
+          val failures = results.map(_.map(x => x.left.toOption).filter(_.isDefined))
+          failures.map { f =>
+            if (f.nonEmpty) {
+              Left(FailedMessage(s"Failed to import datastore $failures"))
+            } else {
+              Right(SuccessMessage(s"Successfully imported files from $x"))
+            }
+          } pipeTo sender
       }
 
     case UpdateJobOutputDir(uuid, path) =>
+      // TODO(smcclellan): Why is this commented out? Can this message be removed?
       //dao.updateJobOutputDir(uuid, path)
       sender ! Right(SuccessMessage(s"Successfully updated jobOptions directory ${uuid.toString} dir ${path.toAbsolutePath}"))
 
