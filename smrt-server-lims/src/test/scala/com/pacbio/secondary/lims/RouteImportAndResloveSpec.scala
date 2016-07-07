ad364ab0b120e424d85caf82ba5974c6cba90aee
Jayson Falkner <jfalkner@gmail.com>
2016-07-07 16:41:01 -0400

    Formatting

--- smrt-server-lims/src/test/scala/com/pacbio/secondary/lims/RouteImportAndResloveSpec.scala
+++ smrt-server-lims/src/test/scala/com/pacbio/secondary/lims/RouteImportAndResloveSpec.scala
@@ -30,27 +30,24 @@ class RouteImportAndResloveSpec extends Specification with Specs2RouteTest with 
       val uuid = "1695780a2e7a0bb7cb1e186a3ee01deb"
       val content =
         s"""expcode: 3220001
-         |runcode: '3220001-0006'
-         |path: 'file:///pbi/collections/322/3220001/r54003_20160212_165105/1_A01'
-         |user: 'MilhouseUser'
-         |uid: '$uuid'
-         |tracefile: 'm54003_160212_165114.trc.h5'
-         |description: 'TestSample'
-         |wellname: 'A01'
-         |cellbarcode: '00000133635908926745416610'
-         |seqkitbarcode: '002222100620000123119'
-         |cellindex: 0
-         |colnum: 0
-         |samplename: 'TestSample'
-         |instid: 90""".stripMargin
+            |runcode: '3220001-0006'
+            |path: 'file:///pbi/collections/322/3220001/r54003_20160212_165105/1_A01'
+            |user: 'MilhouseUser'
+            |uid: '$uuid'
+            |tracefile: 'm54003_160212_165114.trc.h5'
+            |description: 'TestSample'
+            |wellname: 'A01'
+            |cellbarcode: '00000133635908926745416610'
+            |seqkitbarcode: '002222100620000123119'
+            |cellindex: 0
+            |colnum: 0
+            |samplename: 'TestSample'
+            |instid: 90""".stripMargin
 
       // post the data from the file
-    val
-      httpEntity = HttpEntity(MediaTypes.`multipart/form-data`, HttpData(content)).asInstanceOf[HttpEntity.NonEmpty]
-    val
-    formFile = FormFile("file", httpEntity)
-    val
-    mfd = MultipartFormData(Seq(BodyPart(formFile, "file")))
+      val httpEntity = HttpEntity(MediaTypes.`multipart/form-data`, HttpData(content)).asInstanceOf[HttpEntity.NonEmpty]
+      val formFile = FormFile("file", httpEntity)
+      val mfd = MultipartFormData(Seq(BodyPart(formFile, "file")))
       Post("/import", mfd) ~> sealRoute(importLimsYmlRoutes) ~> check {
 
         // confirm that the POST worked without error
@@ -67,14 +64,14 @@ class RouteImportAndResloveSpec extends Specification with Specs2RouteTest with 
         Get(s"/resolve?q=$uuid") ~> sealRoute(resolveRoutes) ~> check {
           response.status.isSuccess mustEqual true
           uuid mustEqual response.entity.data.asString
-      }
+        }
 
         // resolve partial UUID
         Get(s"/resolve?q=${uuid.substring(0, 6)}") ~> sealRoute(resolveRoutes) ~> check {
           response.status.isSuccess mustEqual true
           uuid mustEqual response.entity.data.asString
+        }
       }
     }
-  }
   }
 }
