8b27ae317be0f8c4c9c6f4cd2eed5e68194ab3cf
Jayson Falkner <jfalkner@gmail.com>
2016-07-11 12:43:53 -0400

    DatabaseService usage docs

--- smrt-server-lims/src/main/scala/com/pacbio/secondary/lims/database/DatabaseService.scala
+++ smrt-server-lims/src/main/scala/com/pacbio/secondary/lims/database/DatabaseService.scala
@@ -6,7 +6,34 @@ import com.pacbio.secondary.lims.LimsYml
 /**
  * All of the public DAO methods
  *
- * This trait is the main DB-agnostic interface that the rest of the codebase relies on.
+ * This trait is the main DB-agnostic interface that the rest of the codebase relies on. The Cake
+ * patter is used to build up the desired DB implementation at runtime.
+ *
+ * ## Typical Production Use
+ *
+ * class MyClass extends DefaultDatabaseService {
+ *   //.. do stuff that uses the db -- don't care about backend, use current
+ *   getByUUID(...)
+ *
+ *
+ * ## Production Example (Assuming H2 a JDBC-based RDBMS)
+ *
+ * class MyClass
+ *     extends JdbcDatabaseService // specify JDBC URL
+ *     extends H2DatabaseService { // use the H2 backend
+ *   jdbcUrl = "jdbc:h2:./lims;DB_CLOSE_DELAY=3"
+ *
+ *   //.. do stuff that uses the db
+ *   getByUUID(...)
+ *
+ *
+ * ## Test Use
+ *
+ * class MySpec extends TestDatabaseService { // makes a new in-memory instance for just this test
+ *   //.. do stuff that uses the db
+ *   getByUUID(...)
+ *
+ *
  */
 trait DatabaseService {
 
