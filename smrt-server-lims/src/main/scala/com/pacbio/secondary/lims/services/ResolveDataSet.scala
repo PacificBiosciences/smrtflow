74a8e94eb928f40b499f13bd939e752e47eb2913
Jayson Falkner <jfalkner@gmail.com>
2016-07-12 12:45:01 -0400

    Cleanup LimsSubreadSet code

--- smrt-server-lims/src/main/scala/com/pacbio/secondary/lims/services/ResolveDataSet.scala
+++ smrt-server-lims/src/main/scala/com/pacbio/secondary/lims/services/ResolveDataSet.scala
@@ -4,8 +4,9 @@ import com.pacbio.secondary.lims.LimsYml
 import com.pacbio.secondary.lims.database.Database
 import spray.routing.HttpService
 
-import scala.concurrent.{Await, Future}
-import scala.util.{Failure, Success, Try}
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Success, Try}
 import spray.json._
 import DefaultJsonProtocol._
 import com.pacbio.secondary.lims.JsonProtocol._
@@ -14,12 +15,19 @@ import com.pacbio.secondary.lims.JsonProtocol._
 trait ResolveDataSet extends HttpService {
   this: Database =>
 
-  val resolveRoutes =
-    // LimsSubreadSet query and resolver for alias, experiment id and run code
+  /**
+   * Routes related to resolving LimsSubreadSet as per specification.md
+   *
+   * GET /smrt-lims/lims-subreadset/{Subreadset-UUID} # Returns LimsSubreadSet Resource
+   * GET /smrt-lims/lims-subreadset/{RUN-CODE}        # Returns LimsSubreadSet Resource
+   * GET /smrt-lims/lims-subreadset/{experiment-id}   # Return List of LimsSubreadSet Resource or empty List if exp id isn't found
+   */
+  val resolveDataSetRoutes =
     path("subreadset" / Segment) {
       q => {
         get {
-          resolve(q).map(m => m) match {
+          // serialize as JSON with correct HTTP status code
+          resolveLimsSubreadSet (q) match {
             case lys: Seq[LimsYml] =>
               complete(if (lys.nonEmpty) 200 else 404, lys.toJson.prettyPrint)
             case t: Throwable => throw t // TODO: better error message for HTTP 500?
@@ -28,10 +36,9 @@ trait ResolveDataSet extends HttpService {
       }
     }
 
-  /**
-   * Resolves a dataset identifier to matching subreadsets
-   */
-  def resolve(q: String): Seq[LimsYml] = {
+  // TODO: move this to an actor, probably too slow to keep on request dispatcher
+  def resolveLimsSubreadSet(q: String): Seq[LimsYml] = {
+    // attempt looking up the various ids
     Try(getByExperiment(q.toInt)) match {
       case Success(id :: ids) => getLimsYml(id :: ids)
       case _ =>
@@ -39,10 +46,12 @@ trait ResolveDataSet extends HttpService {
           case Success(id :: ids) => getLimsYml(id :: ids)
           case _ =>
             Try(getByAlias(q)) match {
-            case Success(id) => Seq(getLimsYml(id))
-            case _ => Seq()
-          }
+              case Success(id) => Seq(getLimsYml(id))
+              case _ => Seq[LimsYml]()
+            }
         }
     }
   }
+
+  val resolveRoutes = resolveDataSetRoutes
 }
