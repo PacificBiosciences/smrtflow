b80399b500b948045c19f8adb6fccc667512ac79
Steve McClellan <smcclellan@pacificbiosciences.com>
2016-07-13 18:31:57 -0700

    Remove unused import

--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/models/DataModelParser.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/models/DataModelParser.scala
@@ -9,7 +9,7 @@ import javax.xml.datatype.XMLGregorianCalendar
 import com.pacbio.common.dependency.Singleton
 import com.pacbio.common.services.PacBioServiceErrors.UnprocessableEntityError
 import com.pacbio.common.time.PacBioDateTimeFormat
-import com.pacificbiosciences.pacbiobasedatamodel.{RecordedEventType, SupportedAcquisitionStates}
+import com.pacificbiosciences.pacbiobasedatamodel.SupportedAcquisitionStates
 import com.pacificbiosciences.pacbiodatamodel.PacBioDataModel
 import org.joda.time.{DateTime => JodaDateTime}
 
