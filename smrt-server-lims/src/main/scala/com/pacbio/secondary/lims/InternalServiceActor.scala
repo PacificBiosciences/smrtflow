2b11c0de6b1b3fb339c60425f942658858423843
Jayson Falkner <jfalkner@gmail.com>
2016-07-15 17:50:46 -0400

    StatusService added

--- smrt-server-lims/src/main/scala/com/pacbio/secondary/lims/InternalServiceActor.scala
+++ smrt-server-lims/src/main/scala/com/pacbio/secondary/lims/InternalServiceActor.scala
@@ -1,6 +1,10 @@
 package com.pacbio.secondary.lims
 
 import akka.actor.Actor
+import com.pacbio.common.dependency.Singleton
+import com.pacbio.common.services.utils.StatusGeneratorProvider
+import com.pacbio.common.services.StatusServiceProvider
+import com.pacbio.common.time.{ClockProvider, SystemClockProvider}
 import com.pacbio.secondary.analysis.configloaders.ConfigLoader
 import com.pacbio.secondary.lims.database.{DefaultDatabase, JdbcDatabase}
 import com.pacbio.secondary.lims.services.{ImportLimsYml, ResolveDataSet}
@@ -13,7 +17,14 @@ class InternalServiceActor extends Actor
     with JdbcDatabase
     with DefaultDatabase
     with ImportLimsYml
-    with ResolveDataSet {
+    with ResolveDataSet
+    // rest are to add the status service
+    with StatusServiceProvider
+    with StatusGeneratorProvider
+    with SystemClockProvider {
+  // required for StatusGeneratorProvider
+  lazy override val buildPackage: Singleton[Package] = Singleton(getClass.getPackage)
+  lazy override val baseServiceId: Singleton[String] = Singleton("smrt-server-lims")
 
   lazy val jdbcUrl : String = conf.getString("smrt-server-lims.jdbc-url") // required for JdbcDatabaseService
 
@@ -24,5 +35,5 @@ class InternalServiceActor extends Actor
   // this actor only runs our route, but you could add
   // other things here, like request stream processing
   // or timeout handling
-  def receive = runRoute(importLimsYmlRoutes ~ resolveRoutes)
+  def receive = runRoute(importLimsYmlRoutes ~ resolveRoutes ~ statusService().routes)
 }
