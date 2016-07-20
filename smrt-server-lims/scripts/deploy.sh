a4af482d1b08e614a7e746e1809c0b576154b7b6
Jayson Falkner <jfalkner@gmail.com>
2016-07-20 14:43:43 -0400

    s/GIT_DIR/WORK_DIR

--- smrt-server-lims/scripts/deploy.sh
+++ smrt-server-lims/scripts/deploy.sh
@@ -5,17 +5,18 @@ set -e
 export PATH="/mnt/software/s/sbt/0.13.8/bin:/mnt/software/j/jdk/1.8.0_20/bin:/mnt/software/g/git/2.8.3/ubuntu-1404/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 # create repo if needed
-GIT_DIR="/home/jfalkner/smrt-lims"
-if [ ! -d "$GIT_DIR" ]; then
-   mkdir $GIT_DIR
-   cd $GIT_DIR
+WORK_DIR="/home/jfalkner/smrt-lims"
+if [ ! -d "$WORK_DIR" ]; then
+   mkdir $WORK_DIR
+   cd $WORK_DIR
    git clone https://github.com/PacificBiosciences/smrtflow.git
 fi
 
 # switch to the branch with the latest smrt-lims code
-cd $GIT_DIR/smrtflow
+cd $WORK_DIR/smrtflow
 git checkout smrt-server-lims
 git pull
 
-# entry point for the codebase
-sbt smrt-server-lims/run
+# assemble the JAR for
+sbt smrt-server-lims/assembly
+java -Dconfig.file=$WORK_DIR/production.conf -Xmx2g -jar smrt-server-lims/target/scala-2.11/smrt-server-lims*.jar --debug
