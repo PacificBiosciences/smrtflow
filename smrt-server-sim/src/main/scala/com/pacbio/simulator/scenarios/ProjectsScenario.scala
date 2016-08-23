cbf327ad95cddd5d17042063f469886bf8050a33
natechols <nathaniel.echols@gmail.com>
2016-08-22 17:56:08 -0700

    sim testing for project client calls

--- smrt-server-sim/src/main/scala/com/pacbio/simulator/scenarios/ProjectsScenario.scala
+++ smrt-server-sim/src/main/scala/com/pacbio/simulator/scenarios/ProjectsScenario.scala
@@ -55,6 +55,7 @@ class ProjectsScenario(host: String, port: Int)
   override val smrtLinkClient = new AnalysisServiceAccessLayer(new URL("http", host, port, ""))
 
   val MSG_PROJ_ERR = "Project database should be initially have just one project"
+  val MSG_DS_ERR = "DataSet database should be initially empty"
   val EXIT_SUCCESS: Var[Int] = Var(0)
   val EXIT_FAILURE: Var[Int] = Var(1)
 
@@ -62,10 +63,13 @@ class ProjectsScenario(host: String, port: Int)
   val projId: Var[Int] = Var()
   val projects: Var[Seq[Project]] = Var()
   val project: Var[FullProject] = Var()
-  /*val testdata = PacBioTestData()
+  val testdata = PacBioTestData()
   val ftSubreads = Var(FileTypes.DS_SUBREADS.fileTypeId)
   val subreads1 = Var(testdata.getFile("subreads-xml"))
-  val subreadsUuid1 = Var(dsUuidFromPath(subreads1.get))*/
+  val subreadsUuid1 = Var(dsUuidFromPath(subreads1.get))
+  val subreadSets: Var[Seq[SubreadServiceDataSet]] = Var()
+  val jobId: Var[UUID] = Var()
+  val dsId: Var[Int] = Var()
 
   val setupSteps = Seq(
     jobStatus := GetStatus,
@@ -75,14 +79,19 @@ class ProjectsScenario(host: String, port: Int)
     projects := GetProjects,
     fail(MSG_PROJ_ERR) IF projects.mapWith(_.size) !=? 1,
     projId := CreateProject(Var("test-project"), Var("A test project")),
-    project := GetProject(projId)
-//    fail("Project ID mismatch") IF project.mapWith(_.id) !=? projId.get
-  // TODO add dataset to project
-  /*  subreadSets := GetSubreadSets,
+    project := GetProject(projId),
+    subreadSets := GetSubreadSets,
     fail(MSG_DS_ERR) IF subreadSets ? (_.nonEmpty),
     jobId := ImportDataSet(subreads1, ftSubreads),
     jobStatus := WaitForJob(jobId),
-    fail("Import job failed") IF jobStatus !=? EXIT_SUCCESS,*/
+    fail("Import job failed") IF jobStatus !=? EXIT_SUCCESS,
+    project := GetProject(Var(1)),
+    fail("Expected one dataset in General Project") IF project.mapWith(_.datasets.size) !=? 1,
+    subreadSets := GetSubreadSets,
+    project := UpdateProject(projId, project.mapWith(_.asRequest.appendDataSet(subreadSets.mapWith(_(0).id).get))),
+    fail("Expected one dataset in project") IF project.mapWith(_.datasets.size) !=? 1,
+    project := GetProject(Var(1)),
+    fail("Expected no datasets in General Project") IF project.mapWith(_.datasets.size) !=? 0
   )
   override val steps = setupSteps ++ projectTests
 }
