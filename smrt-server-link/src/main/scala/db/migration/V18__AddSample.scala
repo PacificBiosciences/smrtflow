42c0d5ca4bcfe9653bb311a18f6d20b52609edc9
mpkocher <michael.kocher@me.com>
2016-10-17 15:07:19 -0700

    Add V18 to sync up the missing applied migration from SL 3.1.1

---
+++ smrt-server-link/src/main/scala/db/migration/V18__AddSample.scala
@@ -0,0 +1,29 @@
+package db.migration
+
+
+import scala.concurrent.Future
+
+import slick.driver.SQLiteDriver.api._
+import slick.jdbc.JdbcBackend.DatabaseDef
+
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration
+
+
+/**
+  * Created by mkocher on 10/17/16.
+  *
+  * This is to sync up the missing V7 migration from perforce->github migration during the 3.1.1 release
+  *
+  */
+class V18__AddSample extends JdbcMigration with SlickMigration{
+
+  override def slickMigrate(db: DatabaseDef): Future[Any] = db.run {
+    // scalastyle:off
+    SimpleDBIO {
+      _.connection.prepareCall(
+        """
+CREATE TABLE IF NOT EXISTS "SAMPLE" ("DETAILS" VARCHAR(254) NOT NULL,"UNIQUE_ID" BLOB PRIMARY KEY NOT NULL,"NAME" VARCHAR(254) NOT NULL,"CREATED_BY" VARCHAR(254) NOT NULL,"CREATED_AT" INTEGER NOT NULL);
+        """).execute
+    }
+  }
+}
