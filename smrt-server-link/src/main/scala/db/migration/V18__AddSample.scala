7d0ffc2dfe2c04ba347a3f61e29f938bc3bf27a9
mpkocher <michael.kocher@me.com>
2016-10-17 15:33:36 -0700

    Use the DBIO style.

--- smrt-server-link/src/main/scala/db/migration/V18__AddSample.scala
+++ smrt-server-link/src/main/scala/db/migration/V18__AddSample.scala
@@ -17,13 +17,9 @@ import org.flywaydb.core.api.migration.jdbc.JdbcMigration
   */
 class V18__AddSample extends JdbcMigration with SlickMigration{
 
-  override def slickMigrate(db: DatabaseDef): Future[Any] = db.run {
-    // scalastyle:off
-    SimpleDBIO {
-      _.connection.prepareCall(
-        """
-CREATE TABLE IF NOT EXISTS "SAMPLE" ("DETAILS" VARCHAR(254) NOT NULL,"UNIQUE_ID" BLOB PRIMARY KEY NOT NULL,"NAME" VARCHAR(254) NOT NULL,"CREATED_BY" VARCHAR(254) NOT NULL,"CREATED_AT" INTEGER NOT NULL);
-        """).execute
-    }
+  override def slickMigrate(db: DatabaseDef): Future[Any] = {
+    db.run(DBIO.seq(
+      sqlu"""CREATE TABLE IF NOT EXISTS "SAMPLE" ("DETAILS" VARCHAR(254) NOT NULL,"UNIQUE_ID" BLOB PRIMARY KEY NOT NULL,"NAME" VARCHAR(254) NOT NULL,"CREATED_BY" VARCHAR(254) NOT NULL,"CREATED_AT" INTEGER NOT NULL)"""
+    ))
   }
 }
