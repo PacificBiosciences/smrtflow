5ed4389de2dc13230a5f32119adbede920b95d0a
Steve McClellan <smcclellan@pacificbiosciences.com>
2016-11-03 12:03:40 -0700

    Create SHOULD_RAISE operator for Sim

--- smrt-server-sim/src/main/scala/com/pacbio/simulator/steps/ConditionalSteps.scala
+++ smrt-server-sim/src/main/scala/com/pacbio/simulator/steps/ConditionalSteps.scala
@@ -4,6 +4,7 @@ import com.pacbio.simulator.Scenario
 import com.pacbio.simulator.StepResult._
 
 import scala.concurrent.Future
+import scala.util.control.NonFatal
 
 trait ConditionalSteps {
   this: Scenario with VarSteps =>
@@ -11,6 +12,7 @@ trait ConditionalSteps {
   implicit class ConditionalStep(s: Step) {
     def IF (cond: Var[Boolean]): Step = IfStep(cond, s)
     def SHOULD_FAIL: Step = ExpectFailStep(s)
+    def SHOULD_RAISE: Step = ExpectErrorStep(s)
   }
 
   case class IfStep[T](v: Var[Boolean], step: Step) extends Step {
@@ -26,10 +28,27 @@ trait ConditionalSteps {
     }
   }
 
+  case class ExpectErrorStep(step: Step) extends Step {
+    override val name = s"ExpectError-${step.name}"
+    override def run: Future[Result] = step.run.recoverWith {
+      case NonFatal(ex) => Future { EXCEPTION(ex) }
+    }.map {
+      case EXCEPTION(_, _) => SUCCEEDED
+      case r => FAILED(s"Expected EXCEPTION result but was $r")
+    }
+  }
+
   def fail(failMsg: String): Step = FailStep(failMsg)
 
   case class FailStep[T](failMsg: String) extends Step {
     override val name = "Fail"
     override def run: Future[Result] = Future { FAILED(failMsg) }
+  }
+
+  def exception(ex: Exception): Step = ExceptionStep(ex)
+
+  case class ExceptionStep(ex: Exception) extends Step {
+    override val name = "Exception"
+    override def run: Future[Result] = Future { throw ex }
   }
 }
