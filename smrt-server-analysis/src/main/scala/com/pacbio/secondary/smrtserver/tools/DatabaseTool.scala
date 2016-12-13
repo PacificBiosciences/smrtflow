4713bdeec945cea01d24f03abc18b16f693a7996
mpkocher <michael.kocher@me.com>
2016-12-13 11:20:52 -0800

    Update CLI tool to connect, run migrations and show summary of db.

--- smrt-server-analysis/src/main/scala/com/pacbio/secondary/smrtserver/tools/DatabaseTool.scala
+++ smrt-server-analysis/src/main/scala/com/pacbio/secondary/smrtserver/tools/DatabaseTool.scala
@@ -8,6 +8,8 @@ import com.pacbio.secondary.analysis.configloaders.EngineCoreConfigLoader
 import com.pacbio.secondary.analysis.jobs.PacBioIntJobResolver
 import com.pacbio.secondary.analysis.tools.{CommandLineToolRunner, ToolFailure, ToolSuccess}
 import com.pacbio.secondary.smrtlink.actors.JobsDao
+import com.pacbio.secondary.smrtlink.database.Migrator
+import com.typesafe.config.ConfigFactory
 import org.joda.time.{DateTime => JodaDateTime}
 import scopt.OptionParser
 
@@ -16,29 +18,58 @@ import scala.concurrent.duration.Duration
 import slick.driver.PostgresDriver.api._
 
 
-case class DatabaseToolOptions(uri: URI) extends LoggerConfig
+case class DatabaseToolOptions(username: String,
+                               password: String,
+                               dbName: String,
+                               server: String,
+                               port: Int = 5432) extends LoggerConfig
 
 object DatabaseTool extends CommandLineToolRunner[DatabaseToolOptions] with EngineCoreConfigLoader{
 
   val toolId = "pbscala.tools.database_tool"
-  val VERSION = "0.1.0"
+  val VERSION = "0.2.0"
   val DESCRIPTION =
     """
-      |Display a terse of the contents of the database.
+      |Test connection and Run Database Migrations
     """.stripMargin
 
-  def toS(sx: String) = if (sx.startsWith("jdbc:sqlite:")) sx else s"jdbc:sqlite:$sx"
-  def toURI(sx: String) = new URI(toS(sx))
+  // For the Postgres.app the defaults are
+  // username is "postgres"
+  // password is ""
+  // server is "localhost"
+  // database name is the username ($USER)
+  // Need to think about how to connect this with the application.conf IO layer that
+  // loads the db configuration
+  val defaults = DatabaseToolOptions("postgres", "", "smrtlinkdb", "localhost")
 
+  def toS(opt: DatabaseToolOptions): String =
+    s"""
+      |smrtflow {
+      |  db {
+      |    dataSourceClass = "org.postgresql.ds.PGSimpleDataSource"
+      |    properties = {
+      |      databaseName = ${opt.dbName}
+      |      user = ${opt.username}
+      |      password = "${opt.password}"
+      |      port = ${opt.port}
+      |    }
+      |    numThreads = 10
+      |  }
+      |}
+    """.stripMargin
 
-  lazy val defaultURI =  toURI(conf.getString("pb-services.db-uri"))
-  val defaults = DatabaseToolOptions(defaultURI)
+  def toConfig(opt: DatabaseToolOptions) = ConfigFactory.load(toS(opt))
 
+  def toDefault(s: String) = s"(default: '$s')"
 
   val parser = new OptionParser[DatabaseToolOptions]("database-tools") {
     head("")
     note(DESCRIPTION)
-    opt[String]("db-uri") action { (x, c) => c.copy(uri = toURI(x))}
+    opt[String]('u', "user").action { (x, c) => c.copy(username = x)}.text(s"Database user name ${toDefault(defaults.username)}")
+    opt[String]('p', "password").action {(x, c) => c.copy(password = x)}.text(s"Database Password ${toDefault(defaults.password)}")
+    opt[String]('s', "server").action {(x, c) => c.copy(server = x)}.text(s"Database server ${toDefault(defaults.server)}")
+    opt[String]('n', "db-name").action {(x, c) => c.copy(dbName = x)}.text(s"Database Name ${toDefault(defaults.dbName)}")
+    opt[Int]("port").action {(x, c) => c.copy(port = x)}.text(s"Database port ${toDefault(defaults.port.toString)}")
 
     LoggerOptions.add(this.asInstanceOf[OptionParser[LoggerConfig]])
   }
@@ -46,16 +77,29 @@ object DatabaseTool extends CommandLineToolRunner[DatabaseToolOptions] with Engi
   def run(c: DatabaseToolOptions): Either[ToolFailure, ToolSuccess] = {
     val startedAt = JodaDateTime.now()
 
+    // The migrations are applied outside of the Database.fromConfig model.
+    val m = new Migrator(c.dbName, c.username, c.password, c.server, c.port)
+
+    println(s"Attempting to connect to db with $c")
+    val message = m.testConnection()
+    println(message)
+
+    val result = m.migrate()
+    println(s"Number of successfully applied migrations $result")
+
+    //FIXME(mpkocher)(2016-12-13) Requiring the JobsDao to have the jobResolver is not awesome
     val jobResolver = new PacBioIntJobResolver(Paths.get(engineConfig.pbRootJobDir))
-    val db = Database.forURL(c.uri.toString, driver="org.postgresql.Driver")
+
+    val db = Database.forConfig("smrtflow.db", config = toConfig(c))
+
     // This is not great that engine config and job resolver is
     // required to get the Summary of the database
     val jobsDao = new JobsDao(db, engineConfig, jobResolver)
-
-    val result = Await.result(jobsDao.getSystemSummary(), Duration.Inf)
-
+    val summary = Await.result(jobsDao.getSystemSummary(), Duration.Inf)
     println(s"Summary for $db")
-    println(result)
+    println(summary)
+
+    db.close()
 
     Right(ToolSuccess(toolId, computeTimeDelta(JodaDateTime.now(), startedAt)))
   }
