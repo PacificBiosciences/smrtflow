d1d9e70383cff76cc997b3f187ab677ca942cbbd
mpkocher <michael.kocher@me.com>
2016-12-16 14:38:47 -0800

    Ignore SQL dropping case on initialization system when the tables don't exist.

--- smrt-server-link/src/test/scala/TestUtils.scala
+++ smrt-server-link/src/test/scala/TestUtils.scala
@@ -1,4 +1,5 @@
 import java.nio.file.{Files, Path}
+import java.sql.SQLException
 
 import com.pacbio.secondary.smrtlink.database.{DatabaseConfig, DatabaseUtils}
 import com.typesafe.scalalogging.LazyLogging
@@ -19,16 +20,24 @@ trait TestUtils extends DatabaseUtils with LazyLogging{
     * Drop the SMRT Link tables and flyway version_schema table
     * and recreate and run migrations.
     *
+    * If the initial tables don't exists, they will created, but
+    * a warning will be logged.
+    *
     * @param config Database configuration
     */
   def setupDb(config: DatabaseConfig): Unit = {
-    logger.info(s"Attempting setting up db $config")
+    logger.info(s"Attempting setting up db $config with URI ${config.jdbcURI}")
     val db = config.toDatabase
     val defaultTimeOut = 10.seconds
 
+    def ignoreWithMessage(msg: String): PartialFunction[Throwable, Future[String]] = {
+      case ex: SQLException => Future { s"$msg ${ex.getMessage}" }
+    }
+
     val runner = for {
-      m1 <- dropTables(db)
-      m2 <- dropFlywayTable(db)
+      _ <- Future { TestConnection(config.toDataSource) }
+      m1 <- dropTables(db).recoverWith(ignoreWithMessage("Warning unable to drop smrtlink tables"))
+      m2 <- dropFlywayTable(db).recoverWith(ignoreWithMessage("Warning unable to delete flyway table"))
       m3 <- Future { Migrator(config.toDataSource) }.map(n => s"$n migrations applied")
     } yield Seq(m1, m2, m3).reduce(_ ++ _)
 
