86f50e459e19cd5dac65b6cd17b0f16a0fc98005
mpkocher <michael.kocher@me.com>
2016-12-16 10:19:04 -0800

    Update db CLI tool to use DatabaseUtils

--- smrt-server-analysis/src/main/scala/com/pacbio/secondary/smrtserver/tools/DatabaseTool.scala
+++ smrt-server-analysis/src/main/scala/com/pacbio/secondary/smrtserver/tools/DatabaseTool.scala
@@ -8,13 +8,13 @@ import com.pacbio.secondary.analysis.configloaders.EngineCoreConfigLoader
 import com.pacbio.secondary.analysis.jobs.PacBioIntJobResolver
 import com.pacbio.secondary.analysis.tools.{CommandLineToolRunner, ToolFailure, ToolSuccess}
 import com.pacbio.secondary.smrtlink.actors.JobsDao
-import com.pacbio.secondary.smrtlink.database.Migrator
-import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
+import com.pacbio.secondary.smrtlink.database.{DatabaseConfig, DatabaseUtils}
 import org.joda.time.{DateTime => JodaDateTime}
 import scopt.OptionParser
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
 import slick.driver.PostgresDriver.api._
 
 
@@ -25,6 +25,8 @@ case class DatabaseToolOptions(username: String,
                                port: Int = 5432) extends LoggerConfig
 
 object DatabaseTool extends CommandLineToolRunner[DatabaseToolOptions] with EngineCoreConfigLoader{
+
+  import DatabaseUtils._
 
   val toolId = "pbscala.tools.database_tool"
   val VERSION = "0.2.0"
@@ -57,25 +59,26 @@ object DatabaseTool extends CommandLineToolRunner[DatabaseToolOptions] with Engi
   }
 
   def run(c: DatabaseToolOptions): Either[ToolFailure, ToolSuccess] = {
+
+    val dbConfig = DatabaseConfig(c.dbName, c.username, c.password, c.server, c.port)
     val startedAt = JodaDateTime.now()
 
-    // The migrations are applied outside of the Database.fromConfig model.
-    val m = new Migrator(c.dbName, c.username, c.password, c.server, c.port)
+    TestConnection(dbConfig.toDataSource)
 
-    println(s"Attempting to connect to db with $c")
-    val message = m.testConnection()
+    println(s"Attempting to connect to db with $dbConfig")
+    val message = TestConnection(dbConfig.toDataSource)
     println(message)
 
-    val result = m.migrate()
+    val result = Migrator(dbConfig.toDataSource)
     println(s"Number of successfully applied migrations $result")
 
     //FIXME(mpkocher)(2016-12-13) Requiring the JobsDao to have the jobResolver is not awesome
     val jobResolver = new PacBioIntJobResolver(Paths.get(engineConfig.pbRootJobDir))
 
-    val dbURL = s"jdbc:postgresql://${c.server}:${c.port}/${c.dbName}?user=${c.username}&password=${c.password}"
+    val dbURI = dbConfig.jdbcURI
+    println(s"Postgres URL '$dbURI'")
 
-    println(s"Postgres URL '$dbURL'")
-    val db = Database.forURL(dbURL, driver = "org.postgresql.Driver")
+    val db = dbConfig.toDatabase
 
     // This is not great that engine config and job resolver is
     // required to get the Summary of the database
