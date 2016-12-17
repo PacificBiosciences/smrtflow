a94701b940b4186cd0afb18d52cfd6981cc9d1fc
mpkocher <michael.kocher@me.com>
2016-12-16 17:18:55 -0800

    Add test user ability to login to db.

--- extras/test-db-init.sql
+++ extras/test-db-init.sql
@@ -1,9 +1,10 @@
 -- This should be run to initialize the Test database
 
 CREATE ROLE smrtlink_test_user WITH PASSWORD 'password';
+-- Note, "CREATE USER" is an alias for "CREATE ROLE"
+-- CREATE USER smrtlink_test_user WITH PASSWORD 'password';
 
--- Is this the same as create ROLE ?
-CREATE USER smrtlink_test_user WITH PASSWORD 'password';
+ALTER ROLE smrtlink_test_user WITH LOGIN;
 
 CREATE DATABASE smrtlink_test WITH OWNER smrtlink_test_user;
 
