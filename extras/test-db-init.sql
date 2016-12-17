1162f164ad4bae78218e8ace82d784846f067c7d
mpkocher <michael.kocher@me.com>
2016-12-16 16:49:53 -0800

    Improve test-db-init.sql for circle.

--- extras/test-db-init.sql
+++ extras/test-db-init.sql
@@ -1,7 +1,10 @@
 -- This should be run to initialize the Test database
+
+CREATE ROLE smrtlink_test_user WITH PASSWORD 'password';
+
+-- Is this the same as create ROLE ?
 CREATE USER smrtlink_test_user WITH PASSWORD 'password';
 
-CREATE DATABASE smrtlink_test WITH OWNER smrtlink_user;
+CREATE DATABASE smrtlink_test WITH OWNER smrtlink_test_user;
 
--- Is this correct?
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO smrtlink_test_user;
+GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA smrtlink_test.public TO smrtlink_test_user;
