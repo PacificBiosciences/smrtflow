f9cdf5c6b929e18aaa601d049b5c018b13417b24
mpkocher <michael.kocher@me.com>
2017-02-08 17:52:20 -0800

    Add build number to the version (if bamboo.buildNumber is defined). Update semver format to use "+"
    If build number is set => {major}.{minor}.{patch}+{build-number}.{short-sha}
    else {major}.{minor}.{patch}+{short-sha}

--- smrt-common-models/src/test/scala/SanitySpec.scala
+++ smrt-common-models/src/test/scala/SanitySpec.scala
@@ -1,5 +1,6 @@
-import com.pacificbiosciences.pacbiocollectionmetadata.Primary.OutputOptions.TransferResource
 import org.specs2.mutable.Specification
+
+import com.pacbio.common.models.Constants
 
 /**
  * Sanity test for checking the Data model generated from XSD
@@ -7,18 +8,11 @@ import org.specs2.mutable.Specification
  */
 class SanitySpec extends Specification{
 
-  "Sanity test for loading SubreadSet" should {
-    "Should be able to load" in {
+  "Sanity test for getting the SMRTFlow version" should {
+    "Should be able to load the Smrtflow version" in {
+      val smrtflowVersion = Constants.SMRTFLOW_VERSION
+      println(s"Smrtflow Version $smrtflowVersion")
       true should beTrue
-    }
-    "Test TransferResource" in {
-
-       val tr = new TransferResource()
-       tr.setDescription("A Description")
-       tr.setName("Name of Transfer Resource")
-       tr.setId("My-id")
-       tr.setTransferScheme("NFS")
-      true must beTrue
     }
   }
 }
