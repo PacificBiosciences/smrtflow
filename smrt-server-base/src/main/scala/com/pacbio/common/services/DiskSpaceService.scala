9fcab67d6cefb0c4d6b534c0972019617fd10d3f
Steve McClellan <smcclellan@pacificbiosciences.com>
2017-02-21 10:13:03 -0800

    Flesh out DiskSpaceServiceSpec

--- smrt-server-base/src/main/scala/com/pacbio/common/services/DiskSpaceService.scala
+++ smrt-server-base/src/main/scala/com/pacbio/common/services/DiskSpaceService.scala
@@ -3,20 +3,24 @@ package com.pacbio.common.services
 import java.nio.file.{Paths, Path}
 
 import akka.util.Timeout
-import com.pacbio.common.dependency.Singleton
+import com.pacbio.common.dependency.{ConfigProvider, Singleton}
 import com.pacbio.common.file.{FileSystemUtilProvider, FileSystemUtil}
 import com.pacbio.common.models._
 import com.pacbio.common.services.PacBioServiceErrors.ResourceNotFoundError
-import com.pacbio.secondary.analysis.configloaders.EngineCoreConfigLoader
+import com.pacbio.secondary.analysis.configloaders.{EngineCoreConfigConstants, EngineCoreConfigLoader}
+import com.typesafe.config.Config
 import spray.httpx.SprayJsonSupport._
 import spray.json._
 import DefaultJsonProtocol._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 // TODO(smcclellan): Unit tests
 
-class DiskSpaceService(fileSystemUtil: FileSystemUtil) extends BaseSmrtService with EngineCoreConfigLoader {
+class DiskSpaceService(config: Config, fileSystemUtil: FileSystemUtil)
+  extends BaseSmrtService with EngineCoreConfigLoader {
 
   import PacBioJsonProtocol._
 
@@ -27,12 +31,15 @@ class DiskSpaceService(fileSystemUtil: FileSystemUtil) extends BaseSmrtService w
     "Disk Space Service",
     "0.1.0", "Disk Space Service")
 
+  // We use the injected config here, instead of engineConfig.pbRootJobDir, so tests can use a custom job dir
+  val jobRootDir = loadJobRoot(config.getString(EngineCoreConfigConstants.PB_ROOT_JOB_DIR))
+
   private val idsToPaths: Map[String, Path] = Map(
     "smrtlink.resources.root" -> Paths.get("/"),
-    "smrtlink.resources.jobs_root" -> engineConfig.pbRootJobDir
+    "smrtlink.resources.jobs_root" -> jobRootDir
   )
 
-  private def toResource(id: String): DiskSpaceResource = {
+  private def toResource(id: String): Future[DiskSpaceResource] = Future {
     idsToPaths.get(id).map { p =>
       DiskSpaceResource(
         fileSystemUtil.getFile(p).getAbsolutePath,
@@ -50,7 +57,7 @@ class DiskSpaceService(fileSystemUtil: FileSystemUtil) extends BaseSmrtService w
         get {
           complete {
             ok {
-              idsToPaths.keySet.map(toResource)
+              Future.sequence(idsToPaths.keySet.map(toResource))
             }
           }
         }
@@ -71,16 +78,16 @@ class DiskSpaceService(fileSystemUtil: FileSystemUtil) extends BaseSmrtService w
  * Provides a singleton SpaceService, and also binds it to the set of total services.
  */
 trait DiskSpaceServiceProvider {
-  this: FileSystemUtilProvider =>
+  this: ConfigProvider with FileSystemUtilProvider =>
 
   val diskSpaceService: Singleton[DiskSpaceService] =
-    Singleton(() => new DiskSpaceService(fileSystemUtil())).bindToSet(AllServices)
+    Singleton(() => new DiskSpaceService(config(), fileSystemUtil())).bindToSet(AllServices)
 }
 
 trait DiskSpaceServiceProviderx {
-  this: FileSystemUtilProvider with ServiceComposer =>
+  this: ConfigProvider with FileSystemUtilProvider with ServiceComposer =>
 
-  val diskSpaceService: Singleton[DiskSpaceService] = Singleton(() => new DiskSpaceService(fileSystemUtil()))
+  val diskSpaceService: Singleton[DiskSpaceService] = Singleton(() => new DiskSpaceService(config(), fileSystemUtil()))
 
   addService(diskSpaceService)
 }
