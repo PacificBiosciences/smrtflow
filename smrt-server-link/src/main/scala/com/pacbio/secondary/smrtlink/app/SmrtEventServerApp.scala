8a8ba8980356dfa52df5405aeec452cf5bbf562f
mpkocher <michael.kocher@me.com>
2017-02-28 00:18:50 -0800

    Add upload file service to EventServer.
    - Uploaded files will get copied locally and an SmrtLinkSystemEvent will be returned.
    - Needs more testing
    - Limited to < 8MB (configurable by spray application.conf)

--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/app/SmrtEventServerApp.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/app/SmrtEventServerApp.scala
@@ -1,6 +1,6 @@
 package com.pacbio.secondary.smrtlink.app
 
-import java.io.{BufferedWriter, FileWriter}
+import java.io._
 import java.net.BindException
 import java.nio.file.{Files, Path, Paths}
 import java.util.UUID
@@ -21,25 +21,29 @@ import com.pacbio.secondary.smrtlink.models.{SmrtLinkJsonProtocols, SmrtLinkSyst
 import com.typesafe.scalalogging.LazyLogging
 import spray.can.Http
 import spray.routing.{Route, RouteConcatenation}
+import spray.http._
 import spray.httpx.SprayJsonSupport._
 import spray.json._
 import spray.routing._
 import DefaultJsonProtocol._
+import com.pacbio.common.services.PacBioServiceErrors.{ResourceNotFoundError, UnprocessableEntityError}
 import com.pacbio.logging.LoggerOptions
 import com.pacbio.secondary.analysis.tools.timeUtils
 
 import scala.concurrent._
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.control.NonFatal
 import scala.util.{Failure, Success, Try}
 
 // Jam All the Event Server Components to create a pure Cake (i.e., not Singleton) app
 // in here for first draft.
 
 // Push this back to FileSystemUtils in common
-trait FileUtils {
+trait FileUtils extends LazyLogging{
   def createDirIfNotExists(p: Path): Path = {
     if (!Files.exists(p)) {
+      logger.info(s"Creating dir(s) $p")
       Files.createDirectories(p)
     }
     p
@@ -122,16 +126,29 @@ trait EventServiceBaseMicroService extends PacBioService {
   override def prefixedRoutes = pathPrefix("api" / "v1") { super.prefixedRoutes }
 }
 
-class EventService(eventProcessor: EventProcessor) extends EventServiceBaseMicroService with LazyLogging{
+/**
+  * Event Service for processing SmrtLinkSystemEvents and File uploads
+  *
+  * @param eventProcessor Event Processor, will write, or log system events
+  * @param rootOutputDir root output dir for events
+  * @param rootUploadFilesDir root output dir for files
+  */
+class EventService(eventProcessor: EventProcessor, rootOutputDir: Path, rootUploadFilesDir: Path) extends EventServiceBaseMicroService with LazyLogging with timeUtils{
 
   import SmrtLinkJsonProtocols._
 
   val PREFIX_EVENTS = "events"
+  val PREFIX_FILES = "files"
 
   val manifest = PacBioComponentManifest("events", "Event Services", "0.1.0",
     "SMRT Server Event and general Messages service")
 
   logger.info(s"Creating Service with Event Processor ${eventProcessor.name}")
+
+  def failIfNone[T](message: String): (Option[T] => Future[T]) = {
+    case Some(value) => Future { value}
+    case _ => Future.failed(new UnprocessableEntityError(message))
+  }
 
   def eventRoutes: Route =
     pathPrefix(PREFIX_EVENTS) {
@@ -148,7 +165,159 @@ class EventService(eventProcessor: EventProcessor) extends EventServiceBaseMicro
       }
     }
 
-  def routes = eventRoutes
+  def filesRoute: Route =
+    pathPrefix(PREFIX_FILES) {
+      pathEndOrSingleSlash {
+        post {
+          respondWithMediaType(MediaTypes.`application/json`) {
+            entity(as[MultipartFormData]) { formData =>
+              complete {
+                created {
+                  processUpload(formData)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+  def routes = eventRoutes ~ filesRoute
+
+
+  def createImportEvent(file: File) = {
+    // This isn't quite correct. After the file upload, the system needs to load the tech-support-manifest.json
+    // from the root level of the tgz file and propagate the message. The "message" needs to also have the local
+    // path to the tech-support-manifest file "bundle".
+    SmrtLinkSystemEvent(Constants.SERVER_UUID, "techsupport_import_bundle", 1, UUID.randomUUID(), JodaDateTime.now(), JsObject.empty)
+  }
+
+  /**
+    * Extract the filename to download from the headers
+    *
+    * Assumes only one file is upload
+    *
+    * This NEEDS to be clarified to tightened up. Only load the first
+    * name="techsupport_tgz"; filename="junk.tgz"
+    *
+    * @param headers
+    * @return
+    */
+  def processHeaders(headers: Seq[HttpHeader]): Option[String] = {
+    logger.debug(s"Headers $headers")
+
+    // this is pretty sloppy. The raw header has the form
+    // Content-Disposition: form-data; filename=example.tgz; name=techsupport_tgz
+    def extractFileName(sx: String): Option[String] = {
+      sx.split(";")
+          .map(f => f.trim)
+          .find(_.startsWith("filename="))
+          .flatMap(_.split("filename=").lastOption)
+    }
+
+    headers.find(h => h.is("content-disposition"))
+        .flatMap(x => extractFileName(x.value))
+        .map { fileName =>
+          logger.info(s"Found Filename '$fileName' from headers.")
+          fileName
+        }
+  }
+
+  def isSupportedFileExt(exts: Seq[String], fileName: String): Boolean =
+    exts.map(ext => fileName.endsWith(ext)).reduce(_ || _)
+
+
+  def onlyAllowTarGz(fileName: String): Future[String] = {
+    val supportedExts = Seq(".tgz", ".tar.gz")
+    val errorMessage = s"Invalid file '$fileName'. Supported types ${supportedExts.reduce(_ + ", " + _)}"
+    if (isSupportedFileExt(supportedExts, fileName)) Future(fileName)
+    else Future.failed(throw new UnprocessableEntityError(errorMessage))
+  }
+
+  def resolveOutputFile(fileName: String): File = {
+    val f = rootUploadFilesDir.resolve(fileName).toFile
+    logger.info(s"Resolved $fileName to local output $f")
+    f.createNewFile()
+    logger.info(s"Creating file $f")
+    f
+  }
+
+  def processBodyPart(m: BodyPart, saver: (File, ByteArrayInputStream) => Try[File]): Future[File] = {
+    for {
+      fileName <- failIfNone("Unable to find filename in headers")(processHeaders(m.headers))
+      validFileName <- onlyAllowTarGz(fileName)
+      file <- Future.fromTry(saver(resolveOutputFile(validFileName), new ByteArrayInputStream(m.entity.data.toByteArray)))
+    } yield file
+  }
+
+  def processForm(formData: MultipartFormData, saver: (File, ByteArrayInputStream) => Try[File]): Future[File] = {
+    val bodyPart:Option[BodyPart] = formData.fields.flatMap {
+      case m: BodyPart if processHeaders(m.headers).isDefined => Some(m)
+      case _ => None
+    }.lastOption
+
+    for {
+      body <- failIfNone("Body malformed")(bodyPart)
+      file <- processBodyPart(body, saver)
+    } yield file
+
+  }
+
+  /**
+    * Core Function used within the Route to process the file upload.
+    *
+    * 1. validate filename
+    * 2. save file locally
+    * 3. Create an event for the SL import
+    * 4. Call Event processor
+    *
+    * Consumers of the file upload service should *ONLY* use the companion event as the fundamental interface.
+    *
+    * @param formData
+    * @return
+    */
+  def processUpload(formData: MultipartFormData): Future[SmrtLinkSystemEvent] = {
+    for {
+      file <- processForm(formData, saveAttachmentByteArray)
+      event <- Future { createImportEvent(file)}
+      processedEvent <- eventProcessor.process(event)
+    } yield processedEvent
+  }
+
+  private def saveAttachmentByteArray(outputFile: File, content: ByteArrayInputStream): Try[File] = {
+    saveAttachment[ByteArrayInputStream](outputFile, content,
+      { (is, os) =>
+        val buffer = new Array[Byte](16384)
+        Iterator.continually(is.read(buffer))
+            .takeWhile(_ != -1)
+            .foreach(read => os.write(buffer, 0, read))
+        outputFile
+      }
+    )
+  }
+
+  private def saveAttachment[T](outputFile: File, content: T, writeFile: (T, OutputStream) => File): Try[File] = {
+
+    logger.info(s"Writing to output: $outputFile")
+    val fos = new java.io.FileOutputStream(outputFile)
+
+    val startedAt = JodaDateTime.now()
+
+    Try {
+      writeFile(content, fos)
+      val runTime = computeTimeDeltaFromNow(startedAt)
+      logger.info(s"Successfully processed content (${outputFile.length() / 1024} Kb) in $runTime sec to $outputFile")
+      fos.close()
+      outputFile
+    } match {
+      case Success(f) => Success(f)
+      case Failure(ex) =>
+        fos.close()
+        Failure(ex)
+    }
+
+  }
+
 }
 
 /**
@@ -174,6 +343,8 @@ trait EventServiceConfigCakeProvider extends BaseServiceConfigCakeProvider{
   override lazy val systemName = "smrt-event"
   // This should be loaded from the application.conf with an ENV var mapping
   lazy val eventMessageDir: Path = Paths.get(conf.getString("smrtflow.event.eventRootDir")).toAbsolutePath
+  // Make this independently configurable
+  lazy val eventUploadFilesDir: Path = eventMessageDir.resolve("files")
 }
 
 trait ActorSystemCakeProvider {
@@ -188,7 +359,10 @@ trait EventServicesCakeProvider {
 
   lazy val eventProcessor = new EventFileWriterProcessor(eventMessageDir)
   // All Service instances go here
-  lazy val services: Seq[PacBioService] = Seq(new EventService(eventProcessor), new StatusService(statusGenerator))
+  lazy val services: Seq[PacBioService] = Seq(
+    new EventService(eventProcessor, eventMessageDir, eventUploadFilesDir),
+    new StatusService(statusGenerator)
+  )
 }
 
 trait RootEventServerCakeProvider extends RouteConcatenation{
@@ -225,7 +399,8 @@ trait EventServerCakeProvider extends LazyLogging with timeUtils with FileUtils{
     for {
       validMsg <- validateOption()
       createdDir <- Future {createDirIfNotExists(eventMessageDir)}
-      message <- Future { s"Successfully created $createdDir" }
+      createdFilesDir <- Future {createDirIfNotExists(eventUploadFilesDir)}
+      message <- Future { s"Successfully created $createdDir and $createdFilesDir" }
     } yield s"$validMsg\n$message\nSuccessfully executed preStartUpHook"
 
 
