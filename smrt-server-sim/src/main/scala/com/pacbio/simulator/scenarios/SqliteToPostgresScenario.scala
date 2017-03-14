5ae681d4e94876e0ff0cd0bc41485033e52f7083
Steve McClellan <smcclellan@pacificbiosciences.com>
2017-03-13 18:13:40 -0700

    Initial stub of simulator scenario for sqlite-to-postgres integration testing

---
+++ smrt-server-sim/src/main/scala/com/pacbio/simulator/scenarios/SqliteToPostgresScenario.scala
@@ -0,0 +1,98 @@
+package com.pacbio.simulator.scenarios
+
+import java.io.File
+import java.net.URL
+import java.nio.file.{Paths, Path}
+
+import akka.actor.ActorSystem
+import com.pacbio.secondary.smrtlink.client.SmrtLinkServiceAccessLayer
+import com.pacbio.secondary.smrtlink.database.legacy.{SqliteToPostgresConverter, SqliteToPostgresConverterOptions}
+import com.pacbio.simulator.StepResult.{SUCCEEDED, Result}
+import com.pacbio.simulator.steps.{ConditionalSteps, VarSteps, SmrtLinkSteps}
+import com.pacbio.simulator.{Scenario, ScenarioLoader}
+import com.typesafe.config.{ConfigException, Config}
+
+import scala.concurrent.Future
+
+object SqliteToPostgresScenarioLoader extends ScenarioLoader {
+  override def load(config: Option[Config])(implicit system: ActorSystem): Scenario = {
+    require(config.isDefined, "Path to config file must be specified for SqliteToPostgresScenario")
+    val c: Config = config.get
+
+    // Resolve overrides with String
+    def getInt(key: String): Int =
+      try {
+        c.getInt(key)
+      } catch {
+        case e: ConfigException.WrongType => c.getString(key).trim.toInt
+      }
+
+    val opts = SqliteToPostgresConverterOptions(
+      // TODO(smcclellan): Construct golden sqlite db file (See SL-985)
+      new File(c.getString("sqlite-file")),
+      c.getString("user"),
+      c.getString("password"),
+      c.getString("db-name"),
+      c.getString("server"),
+      getInt("port"))
+
+    new SqliteToPostgresScenario(Paths.get(c.getString("smrt-link-jar-file")), opts)
+  }
+}
+
+class SqliteToPostgresScenario(smrtLinkJar: Path, opts: SqliteToPostgresConverterOptions)
+  extends Scenario with VarSteps with ConditionalSteps with SmrtLinkSteps {
+
+  override val name = "SqliteToPostgresScenario"
+
+  override val smrtLinkClient = new SmrtLinkServiceAccessLayer(new URL("http", "localhost", 8081, ""))
+
+  // TODO(smcclellan): Move these steps into ...simulator.steps package?
+
+  case class RunSqliteToPostgresConverterStep(opts: Var[SqliteToPostgresConverterOptions]) extends Step {
+    override val name = "RunSqliteToPostgresConverter"
+    override def run: Future[Result] = Future {
+      SqliteToPostgresConverter.runImporter(opts.get)
+      SUCCEEDED
+    }
+  }
+
+  case class LaunchSmrtLinkStep(pathToJar: Var[Path]) extends VarStep[Process] {
+    override val name = "LaunchSmrtLink"
+    override def run: Future[Result] = Future {
+      // TODO(smcclellan): Pass in necessary configs with '-D'
+      val cmd = s"java -jar ${pathToJar.get.toAbsolutePath}"
+      output(Runtime.getRuntime.exec(cmd))
+      SUCCEEDED
+    }
+  }
+
+  case class KillSmrtLink(process: Var[Process]) extends VarStep[Int] {
+    override val name = "KillSmrtLink"
+    override def run: Future[Result] = Future {
+      process.get.destroy()
+      output(process.get.exitValue())
+      SUCCEEDED
+    }
+  }
+
+  val converterOpts: Var[SqliteToPostgresConverterOptions] = Var(opts)
+  val smrtLinkJarPath: Var[Path] = Var(smrtLinkJar)
+  val smrtLinkProcess: Var[Process] = Var()
+  val smrtLinkExitCode: Var[Int] = Var()
+
+  override val steps = Seq(
+    // TODO(smcclellan): Add step to run Flyway migrations
+
+    RunSqliteToPostgresConverterStep(converterOpts),
+
+    smrtLinkProcess := LaunchSmrtLinkStep(smrtLinkJarPath),
+
+    // TODO(smcclellan): Add steps to verify that SMRTLink endpoints contain data from SQLite
+
+    // TODO(smcclellan): Ensure this step runs regardless of prior failures
+    smrtLinkExitCode := KillSmrtLink(smrtLinkProcess),
+
+    fail("SMRT Link exited with non-zero code") IF smrtLinkExitCode !=? 0
+  )
+}
