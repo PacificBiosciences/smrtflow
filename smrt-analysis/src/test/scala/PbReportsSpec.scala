308ddb596753f33194f75fdac28da456f2638d72
natechols <nathaniel.echols@gmail.com>
2017-03-16 14:54:14 -0700

    run control report (or fail gently) on imported SubreadSets with stx.xml

--- smrt-analysis/src/test/scala/PbReportsSpec.scala
+++ smrt-analysis/src/test/scala/PbReportsSpec.scala
@@ -1,7 +1,13 @@
 import java.nio.file.{Paths, Path, Files}
-import com.pacbio.secondary.analysis.externaltools.PbReports
+
 import com.typesafe.scalalogging.LazyLogging
 import org.specs2.mutable._
+
+import com.pacbio.secondary.analysis.externaltools.{PbReports, PacBioTestData}
+import com.pacbio.secondary.analysis.reports.DataSetReports
+import com.pacbio.secondary.analysis.jobs.JobModels.JobTypeId
+import com.pacbio.secondary.analysis.jobs.NullJobResultsWriter
+import com.pacbio.secondary.analysis.datasets.DataSetMetaTypes
 
 /**
  * Tests for calling pbreports on an sts.xml file
@@ -20,6 +26,49 @@ class PbReportsSpec extends Specification with LazyLogging {
       val outPath = Paths.get(tmpDir.toString(), "filter.json")
       PbReports.FilterStatsXml.run(Paths.get(stsXml.toURI()), outPath)
       Files.exists(outPath) must beTrue
+    }
+  }
+}
+
+class PbReportsSubreadsSpec extends Specification with LazyLogging {
+
+  args(skipAll = !(PbReports.isAvailable() && PacBioTestData.isAvailable))
+
+  val log = new NullJobResultsWriter
+  "SubreadSet report generation" should {
+    "create three reports for Sequel dataset with sts.xml" in {
+      val pbdata = PacBioTestData()
+      val f = pbdata.getFile("subreads-sequel")
+      val tmpDir = Files.createTempDirectory("reports")
+      val rpts = DataSetReports.runAll(f, DataSetMetaTypes.Subread,
+                                       tmpDir, JobTypeId("reports"), log)
+      rpts.size must beEqualTo(3)
+    }
+    "create a single fallback report for converted RSII dataset" in {
+      val pbdata = PacBioTestData()
+      val f = pbdata.getFile("subreads-xml")
+      val tmpDir = Files.createTempDirectory("reports")
+      val rpts = DataSetReports.runAll(f, DataSetMetaTypes.Subread,
+                                       tmpDir, JobTypeId("reports"), log)
+      rpts.size must beEqualTo(1)
+    }
+  }
+}
+
+// FIXME the dataset needs to go into PacBioTestData, and then we merge this
+// with the previous class
+class PbReportsSubreadsControlSpec extends Specification with LazyLogging {
+  val log = new NullJobResultsWriter
+
+  val DS_PATH = Paths.get("/pbi/dept/secondary/siv/testdata/SA3-Sequel/lambda/314/3140099/r54099_20160830_222805/3_C01_control/m54099_160831_134818.subreadset.xml")
+
+  args(skipAll = !(PbReports.isAvailable() && DS_PATH.toFile.exists))
+  "SubreadSet report generation" should {
+    "create four reports for a newer Sequel dataset with control" in {
+      val tmpDir = Files.createTempDirectory("reports")
+      val rpts = DataSetReports.runAll(DS_PATH, DataSetMetaTypes.Subread,
+                                       tmpDir, JobTypeId("reports"), log)
+      rpts.size must beEqualTo(4)
     }
   }
 }
