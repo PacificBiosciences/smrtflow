56f422dca84a475ff8a28d0601b7da9b897f5118
mpkocher <michael.kocher@me.com>
2017-03-27 18:10:47 -0700

    Update bundle related Specs with new model.

--- smrt-server-link/src/test/scala/PacBioBundleUtilsSpec.scala
+++ smrt-server-link/src/test/scala/PacBioBundleUtilsSpec.scala
@@ -10,7 +10,7 @@ class PacBioBundleUtilsSpec extends Specification{
   "Bundle Utils tests" should {
     "Sort bundles by semVer" in {
 
-      def toBundle(v: String) = PacBioDataBundle("a", v, JodaDateTime.now(), Paths.get("/path/to"), None)
+      def toBundle(v: String) = PacBioDataBundle("a", v, JodaDateTime.now(), None)
 
       val bundles = Seq("1.2.4", "1.0.0", "1.1.0", "1.0.1", "1.3.1").map(toBundle)
 
