2dfaa8c0253c2182cc6f7b1315be4df6eed04353
mpkocher <michael.kocher@me.com>
2017-04-07 15:49:42 -0700

    Update test

--- smrt-server-link/src/test/scala/PacBioBundleUtilsSpec.scala
+++ smrt-server-link/src/test/scala/PacBioBundleUtilsSpec.scala
@@ -1,7 +1,5 @@
-import java.nio.file.Paths
-
+import com.pacbio.secondary.smrtlink.io.PacBioDataBundleIOUtils
 import com.pacbio.secondary.smrtlink.models.PacBioDataBundle
-import com.pacbio.secondary.smrtlink.services.BundleUtils
 import org.specs2.mutable.Specification
 import org.joda.time.{DateTime => JodaDateTime}
 
@@ -14,7 +12,7 @@ class PacBioBundleUtilsSpec extends Specification{
 
       val bundles = Seq("1.2.4", "1.0.0", "1.1.0", "1.0.1", "1.3.1").map(toBundle)
 
-      val newestBundle = BundleUtils.getNewestBundleVersionByType(bundles, "a")
+      val newestBundle = PacBioDataBundleIOUtils.getNewestBundleVersionByType(bundles, "a")
 
       newestBundle.map(_.version) must beSome("1.3.1")
     }
