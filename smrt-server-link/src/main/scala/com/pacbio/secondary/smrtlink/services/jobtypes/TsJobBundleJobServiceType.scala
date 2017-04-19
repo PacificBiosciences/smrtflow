018a96307e3c36526f415a1583d0e261f316df16
mpkocher <michael.kocher@me.com>
2017-04-18 18:15:30 -0700

    Cleanup

--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/services/jobtypes/TsJobBundleJobServiceType.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/services/jobtypes/TsJobBundleJobServiceType.scala
@@ -25,37 +25,36 @@ import com.pacbio.secondary.smrtlink.services.JobManagerServiceProvider
 import org.joda.time.{DateTime => JodaDateTime}
 
 
-class TsJobBundleJobServiceType(dbActor: ActorRef, authenticator: Authenticator, smrtLinkVersion: Option[String]) extends {
+class TsJobBundleJobServiceType(dbActor: ActorRef, authenticator: Authenticator, smrtLinkVersion: Option[String], smrtLinkSystemId: UUID, dnsName: Option[String]) extends {
   override val endpoint = JobTypeIds.TS_JOB.id
   override val description = "TechSupport Job to create a TGZ bundle of an SMRT Link Job that can be sent to PacBio for TroubleShooting help"
 } with JobTypeService[TsJobBundleJobServiceOptions](dbActor, authenticator) with ProjectIdJoiner with LazyLogging {
 
   import CommonModelImplicits._
 
-  def toManifest(user: String, comment: String, smrtLinkSystemUUID: UUID, jobId: Int, jobType: String, dnsName: Option[String]): TsJobManifest = {
-    TsJobManifest(UUID.randomUUID(), BundleTypes.JOB, 1, JodaDateTime.now(), smrtLinkSystemUUID,
+  def toTsManifest(manifestUUID: UUID, user: String, comment: String, smrtLinkSystemUUID: UUID, jobId: Int, jobType: String, dnsName: Option[String]): TsJobManifest = {
+    TsJobManifest(manifestUUID, BundleTypes.JOB, 1, JodaDateTime.now(), smrtLinkSystemUUID,
       dnsName, smrtLinkVersion, user, Some(comment), endpoint, jobId)
   }
 
   override def createJob(opts: TsJobBundleJobServiceOptions, user: Option[UserRecord]): Future[CreateJobType] = {
 
-    val jobId = UUID.randomUUID()
-    val name = "Tech Support bundle"
     // This isn't great and might create confusion, but re-using the
     // job uuid as the tech support bundle (and event) UUID might be a good idea
-    val manifestUUID = UUID.randomUUID()
-
-    // mock out to get dependencies correct. These need to be initialized to all core service job init, which
-    // will require all jobs to be initialized using these required system values?
-    val smrtLinkSystemUUID = Constants.SERVER_UUID
-    val dnsName = "UNKNOWN"
+    val jobId = UUID.randomUUID()
 
     for {
       engineJob <- getJob(opts.jobId)
-    } yield CreateJobType(jobId, name, opts.comment, endpoint, CoreJob(jobId,
-      TsJobBundleJobOptions(Paths.get(engineJob.path), toManifest(opts.user, opts.comment, smrtLinkSystemUUID, engineJob.id, engineJob.jobTypeId, Some(dnsName)))),
-      None,
-      opts.toJson.toString(), user.map(_.userId), smrtLinkVersion)
+    } yield CreateJobType(jobId, "Tech Support Job bundle", opts.comment, endpoint,
+      CoreJob(jobId,
+        TsJobBundleJobOptions(
+          Paths.get(engineJob.path),
+          toTsManifest(jobId, opts.user, opts.comment, smrtLinkSystemId, engineJob.id, engineJob.jobTypeId, dnsName)
+        )),
+        None,
+        opts.toJson.toString(),
+        user.map(_.userId),
+        smrtLinkVersion)
   }
 }
 
@@ -65,7 +64,7 @@ trait TsJobBundleJobServiceTypeProvider {
     with JobManagerServiceProvider with SmrtLinkConfigProvider =>
 
   val tsJobBundleJobServiceType: Singleton[TsJobBundleJobServiceType] =
-    Singleton(() => new TsJobBundleJobServiceType(jobsDaoActor(), authenticator(), smrtLinkVersion())).bindToSet(JobTypes)
+    Singleton(() => new TsJobBundleJobServiceType(jobsDaoActor(), authenticator(), smrtLinkVersion(), Constants.SERVER_UUID, dnsName())).bindToSet(JobTypes)
 
 }
 
--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/services/jobtypes/TsSystemStatusBundleServiceType.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/services/jobtypes/TsSystemStatusBundleServiceType.scala
@@ -28,15 +28,15 @@ import com.pacbio.secondary.smrtlink.services.JobManagerServiceProvider
 
 
 
-class TsSystemStatusBundleServiceType(dbActor: ActorRef, authenticator: Authenticator, smrtLinkVersion: Option[String]) extends {
+class TsSystemStatusBundleServiceType(dbActor: ActorRef, authenticator: Authenticator, smrtLinkVersion: Option[String], smrtLinkSystemId: UUID, dnsName: Option[String]) extends {
   override val endpoint = JobTypeIds.TS_SYSTEM_STATUS.id
   override val description = "TechSupport Job to create a TGZ bundle of an SMRT Link System Status that can be sent to PacBio for TroubleShooting help"
 } with JobTypeService[TsSystemStatusServiceOptions](dbActor, authenticator) with ProjectIdJoiner with LazyLogging {
 
   import CommonModelImplicits._
 
-  def toManifest(user: String, comment: String, smrtLinkSystemUUID: UUID, dnsName: Option[String]) = {
-    TsSystemStatusManifest(UUID.randomUUID(), BundleTypes.JOB, 1, JodaDateTime.now(), smrtLinkSystemUUID,
+  def toTsManifest(manifestUUID: UUID, user: String, comment: String, smrtLinkSystemUUID: UUID, dnsName: Option[String]) = {
+    TsSystemStatusManifest(manifestUUID, BundleTypes.SYSTEM_STATUS, 1, JodaDateTime.now(), smrtLinkSystemUUID,
       dnsName, smrtLinkVersion, user, Some(comment))
   }
 
@@ -45,28 +45,22 @@ class TsSystemStatusBundleServiceType(dbActor: ActorRef, authenticator: Authenti
     val smrtLinkSystemRoot: Option[Path] = Some(Paths.get("/"))
     val errorMessage = "System is not configured with SMRT Link System Root. Can NOT create TS Status Job"
 
-    val jobId = UUID.randomUUID()
-    val name = "Tech Support System Status bundle"
     // This isn't great and might create confusion, but re-using the
     // job uuid as the tech support bundle (and event) UUID might be a good idea
-    val manifestUUID = UUID.randomUUID()
+    // Also when the job is created, the UUID of the job can be communicated to the Client (i.e., UI)
+    val jobId = UUID.randomUUID()
 
-    // mock out to get dependencies correct. These need to be initialized to all core service job init, which
-    // will require all jobs to be initialized using these required system values?
-    val smrtLinkSystemUUID = Constants.SERVER_UUID
-    val dnsName = "UNKNOWN"
-
-    // If the system isn't configured, we fail to create the Job
-    val fx = smrtLinkSystemRoot.map { root =>
-      CreateJobType(jobId, name, opts.comment, endpoint, CoreJob(jobId,
-        TsSystemStatusBundleOptions(root, toManifest(opts.user, opts.comment, smrtLinkSystemUUID, Some(dnsName)))),
+    val jobOpt = smrtLinkSystemRoot.map { root =>
+      CreateJobType(jobId, "Tech Support System Status bundle", opts.comment, endpoint, CoreJob(jobId,
+        TsSystemStatusBundleOptions(root, toTsManifest(jobId, opts.user, opts.comment, smrtLinkSystemId, dnsName))),
         None,
         opts.toJson.toString(),
         user.map(_.userId),
         smrtLinkVersion)
     }
 
-    fx.map(j => Future.successful(j))
+    // If the system isn't configured, we fail to create the Job
+    jobOpt.map(j => Future.successful(j))
         .getOrElse(Future.failed(throw new UnprocessableEntityError(errorMessage)))
   }
 }
@@ -77,5 +71,5 @@ trait TsSystemStatusBundleServiceTypeProvider {
       with JobManagerServiceProvider with SmrtLinkConfigProvider =>
 
   val tsSystemStatusBundleJobServiceType: Singleton[TsSystemStatusBundleServiceType] =
-    Singleton(() => new TsSystemStatusBundleServiceType(jobsDaoActor(), authenticator(), smrtLinkVersion())).bindToSet(JobTypes)
+    Singleton(() => new TsSystemStatusBundleServiceType(jobsDaoActor(), authenticator(), smrtLinkVersion(), Constants.SERVER_UUID, dnsName())).bindToSet(JobTypes)
 }
