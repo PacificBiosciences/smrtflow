a49bd977045b376c70a976e250b615b9eb3339b5
mpkocher <michael.kocher@me.com>
2017-06-26 18:11:38 -0700

    Add pbservice cram-ish int test scaffolding

---
+++ smrt-server-sim/src/it/scala/PbServiceIntegrationSpec.scala
@@ -0,0 +1,22 @@
+import com.pacbio.secondary.analysis.externaltools.ExternalToolsUtils
+import org.specs2.mutable.Specification
+
+
+class PbServiceIntegrationSpec extends Specification{
+
+  def toCmd(args: String*): Seq[String] = Seq("pbservice") ++ args
+  // pack should be called and PATH should be exported before these
+  // tests are called
+  "pbservice cram test " should {
+    "exe is found" in {
+      ExternalToolsUtils.which("pbservice") must beSome
+    }
+    "help is working" in {
+      ExternalToolsUtils.runSimpleCmd(toCmd("--help")) must beNone
+    }
+    "version is working" in {
+      ExternalToolsUtils.runSimpleCmd(toCmd("--version")) must beNone
+    }
+  }
+
+}
