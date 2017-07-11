01dee82f7b138301024fba8d1e01884468fa99a0
mpkocher <michael.kocher@me.com>
2017-07-11 01:51:05 -0700

    Add webservice to smrt-server-link
    Migrate alarm mechanism from smrt-server-base to smrt-server-link

---
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/services/AlarmService.scala
@@ -0,0 +1,53 @@
+package com.pacbio.secondary.smrtlink.services
+
+import akka.actor.ActorRef
+import akka.pattern._
+import akka.util.Timeout
+import com.pacbio.common.actors.ActorRefFactoryProvider
+import com.pacbio.secondary.smrtlink.actors.{AlarmDaoActor, AlarmDaoActorProvider}
+import com.pacbio.common.dependency.Singleton
+import com.pacbio.common.models._
+import com.pacbio.common.services.ServiceComposer
+import spray.httpx.SprayJsonSupport._
+import spray.json._
+import spray.routing.PathMatchers.Segment
+
+import scala.concurrent.ExecutionContext.Implicits._
+import scala.concurrent.duration._
+
+class AlarmService(alarmDaoActor: ActorRef) extends SmrtLinkBaseMicroService with DefaultJsonProtocol {
+
+  import PacBioJsonProtocol._
+  import AlarmDaoActor._
+
+  val manifest = PacBioComponentManifest(
+    toServiceId("alarm"),
+    "Alarm Service",
+    "0.1.0", "Subsystem Alarm Service of the Alarm Runners and Alarm Status")
+
+  val alarmServiceName = "alarms"
+
+  // This is really only for debugging to see what Alarm Runners are loaded
+  val alarmStatusServiceName = "alarm-runners"
+
+  val routes =
+    pathPrefix(alarmServiceName) {
+      pathEndOrSingleSlash {
+        get {
+          complete {
+            ok {
+              (alarmDaoActor ? GetAllAlarmStatus).mapTo[Seq[AlarmStatus]]
+            }
+          }
+        }
+      }
+    }
+}
+
+trait AlarmServiceProvider {
+  this: ActorRefFactoryProvider with AlarmDaoActorProvider with ServiceComposer =>
+
+  val alarmService: Singleton[AlarmService] = Singleton(() => new AlarmService(alarmDaoActor()))
+
+  addService(alarmService)
+}
