b87267779138f64c12e3ee35783a9b9457c3049f
mpkocher <michael.kocher@me.com>
2017-07-14 10:16:22 -0700

    Add building of conda env

--- extras/pbbundler/bamboo_build_smrtflow.sh
+++ extras/pbbundler/bamboo_build_smrtflow.sh
@@ -2,13 +2,8 @@
 
 set -o errexit
 set -o pipefail
-# set -o nounset
+set -o nounset
 set -o xtrace
-
-if ! [ "${PGDATA:-}" ] || ! [ "${PGPORT:-}" ]; then
-    echo "PGDATA and PGPORT must be set"
-    exit 1
-fi
 
 export PB_TEST_DATA_FILES=`readlink -f repos/PacBioTestData/data/files.json`
 
@@ -31,3 +26,50 @@ export SMRTFLOW_TEST_DB_PORT=$PGPORT
 
 
 env TMP=`pwd`/tmp sbt -no-colors compile test publish
+
+echo "Loading Conda GNU module"
+module load anaconda
+
+echo "Building Conda Env"
+
+# This only really needs to be done once
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda config --add channels bioconda
+
+# This needs to be host specific?
+env_name="testenv01"
+
+echo "Setting up new env '${env_name}'"
+# how to do you initialize this without any dep?
+conda create --yes -n "${env_name}" numpy cython matplotlib
+
+source activate "${env_name}"
+
+#conda install --yes cython
+#conda install --yes numpy
+#conda install --yes matplotlib
+conda install --yes -c bioconda pysam=0.11.2.2
+
+# Install all PB py dependencies
+pip install -r pbcommand/REQUIREMENTS.txt
+cd pbcommand && pip install . && cd -
+
+pip install -r pbcore/requirements.txt
+cd pbcore && pip install . && cd -
+
+pip install -r pbcoretools/requirements.txt
+cd pbcoretools && pip install . && cd -
+
+cd pbsmrtpipe && pip install . && cd -
+pip install -r pbsmrtpipe/REQUIREMENTS.txt
+
+cd pbreports && pip install . && cd -
+pip install -r pbreports/REQUIREMENTS.txt
+
+# Sanity test
+dataset --help
+pbsmrtpipe --help
+python -m pbreports.report.mapping_stats --help
+
+
