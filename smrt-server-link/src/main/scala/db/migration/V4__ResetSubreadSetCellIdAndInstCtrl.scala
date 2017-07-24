221733222fc77e6e74cc971b353bd577b4b50562
mpkocher <michael.kocher@me.com>
2017-07-24 09:20:46 -0700

    Add reseting of default cell-id and inst-ctl-version from hardcoded values

---
+++ smrt-server-link/src/main/scala/db/migration/V4__ResetSubreadSetCellIdAndInstCtrl.scala
@@ -0,0 +1,32 @@
+package db.migration
+
+import com.typesafe.scalalogging.LazyLogging
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration
+import slick.driver.PostgresDriver.api._
+import slick.jdbc.JdbcBackend.DatabaseDef
+
+import scala.concurrent.Future
+
+// scalastyle:off
+class V4__ResetSubreadSetCellIdAndInstCtrl extends JdbcMigration with SlickMigration with LazyLogging{
+
+  /**
+    * This will update the hardcoded values to "unknown"
+    *
+    * 1. cell_id from "cell-id" value to "unknown"
+    * 2  instrument_control_version from 'instrument-ctr-version to "unknown"
+    *
+    * This will make the unknown values at least be consistent by setting
+    * these old values to "unknown".
+    *
+    * This should have probably be set to Option[String] to avoid this. I don't
+    * recall the historical context. This could be friction with parsing the XML.
+    *
+    */
+  override def slickMigrate(db: DatabaseDef): Future[Any] = {
+    db.run(DBIO.seq(
+      sqlu"""UPDATE dataset_subreads SET cell_id = 'unknown' WHERE cell_id = 'cell-id'""",
+      sqlu"""UPDATE dataset_subreads SET instrument_control_version = 'unknown' WHERE cell_id = 'instrument-ctr-version'"""
+    ))
+  }
+}
