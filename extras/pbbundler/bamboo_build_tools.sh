1fa55001a999c38facaf3b9609d0a48f6b0c8d04
mpkocher <michael.kocher@me.com>
2017-08-16 10:34:18 -0700

    Fix bamboo tools build to use smrt-server-link/pack from smrt-analysis

--- extras/pbbundler/bamboo_build_tools.sh
+++ extras/pbbundler/bamboo_build_tools.sh
@@ -7,12 +7,9 @@ module load sbt
 SHA=`git rev-parse --short HEAD`
 echo SHA is ${SHA}
 rm -f pbscala*.tar.gz
-sbt -no-colors smrt-analysis/pack smrt-server-base/pack smrt-server-link/pack
-cp -r smrt-server-base/target/pack/* smrt-analysis/target/pack/
-cp -r smrt-server-link/target/pack/* smrt-analysis/target/pack/
-rm -rf smrt-analysis/target/pack/bin/*.bat
+sbt -no-colors smrt-server-link/pack
 
 TARBALL=${bamboo_working_directory}/pbscala-packed-${SHA}.tar.gz
 
-tar cvfz $TARBALL smrt-analysis/target/pack \
+tar cvfz $TARBALL smrt-server-link/target/pack \
   && cp $TARBALL /mnt/secondary/Share/pbscala-tools/
