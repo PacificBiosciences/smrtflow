6fbe12acaff8cdc7fbafd76e1c15f424ffa583c7
mpkocher <michael.kocher@me.com>
2017-08-31 14:35:32 -0700

    Add validation to import barcode set (this needs to be further improved)

--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/jobtypes/ImportBarcodeFastaJob.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/jobtypes/ImportBarcodeFastaJob.scala
@@ -1,24 +1,27 @@
 package com.pacbio.secondary.smrtlink.jobtypes
 
-import java.nio.file.{Path, Paths}
+import java.nio.file.{Files, Path, Paths}
 import java.util.UUID
 
 import com.pacbio.secondary.smrtlink.actors.JobsDao
 import com.pacbio.secondary.smrtlink.analysis.jobs.JobModels.JobConstants.GENERAL_PROJECT_ID
 import com.pacbio.secondary.smrtlink.analysis.jobs.JobModels._
-import com.pacbio.secondary.smrtlink.analysis.jobs.JobResultWriter
+import com.pacbio.secondary.smrtlink.analysis.jobs.{InvalidJobOptionError, JobResultWriter}
 import com.pacbio.secondary.smrtlink.analysis.jobtypes.ConvertImportFastaBarcodesOptions
 import com.pacbio.secondary.smrtlink.models.ConfigModels.SystemJobConfig
-
 import spray.json._
 
 //FIXME(mpkocher)(8-17-2017) There's a giant issue with the job "name" versus "name" used in job options.
-case class ImportBarcodeFastaJobOptions(path: String, name: Option[String], description: Option[String],
+case class ImportBarcodeFastaJobOptions(path: Path, name: Option[String], description: Option[String],
                                         projectId: Option[Int] = Some(JobConstants.GENERAL_PROJECT_ID)
                                        ) extends ServiceJobOptions {
   override def jobTypeId = JobTypeIds.CONVERT_FASTA_BARCODES
-  override def validate(dao: JobsDao, config: SystemJobConfig) = None
   override def toJob() = new ImportBarcodeFastaJob(this)
+
+  //(mpkocher)(8-31-2017) This validation needs to be improved.
+  override def validate(dao: JobsDao, config: SystemJobConfig):Option[InvalidJobOptionError] = {
+    if (Files.exists(path)) None else Some(InvalidJobOptionError(s"Unable to find $path"))
+  }
 }
 
 class ImportBarcodeFastaJob(opts: ImportBarcodeFastaJobOptions) extends ServiceCoreJob(opts){
@@ -27,7 +30,7 @@ class ImportBarcodeFastaJob(opts: ImportBarcodeFastaJobOptions) extends ServiceC
     // Shim layer
     val name = opts.name.getOrElse("Fasta-Barcodes")
     val projectId = opts.projectId.getOrElse(GENERAL_PROJECT_ID)
-    val oldOpts = ConvertImportFastaBarcodesOptions(name, opts.path, projectId)
+    val oldOpts = ConvertImportFastaBarcodesOptions(opts.path.toAbsolutePath.toString, name, projectId)
     val job = oldOpts.toJob
     job.run(resources, resultsWriter)
   }
