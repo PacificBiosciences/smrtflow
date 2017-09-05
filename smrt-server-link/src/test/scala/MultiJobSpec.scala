faba432d44f85790e6f1aea06f791f3d226c2398
mpkocher <michael.kocher@me.com>
2017-09-05 14:37:51 -0700

    Add scaffolding for Multijob spec.

---
+++ smrt-server-link/src/test/scala/MultiJobSpec.scala
@@ -0,0 +1,11 @@
+import org.specs2.mutable.Specification
+
+
+class MultiJobSpec extends Specification{
+
+  "Sanity multi job test" should {
+    "create a model" in {
+      true must beTrue
+    }
+  }
+}
