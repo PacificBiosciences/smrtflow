ef7a850506000986f33452d0f260a0479ce569e8
natechols <nathaniel.echols@gmail.com>
2017-09-08 14:15:32 -0700

    restore old authorization mechanism for testing projects

--- smrt-server-sim/src/main/scala/com/pacbio/simulator/scenarios/PbsmrtpipeScenario.scala
+++ smrt-server-sim/src/main/scala/com/pacbio/simulator/scenarios/PbsmrtpipeScenario.scala
@@ -158,7 +158,7 @@ class PbsmrtpipeScenario(host: String, port: Int)
   import JobModels._
 
   override val name = "PbsmrtpipeScenario"
-  override val smrtLinkClient = new SmrtLinkServiceAccessLayer(host, port)
+  override val smrtLinkClient = new SmrtLinkServiceAccessLayer(host, port, Some("jsnow"))
 
   val diagnosticJobTests = Seq(
     projectId := CreateProject(projectName, projectDesc),
