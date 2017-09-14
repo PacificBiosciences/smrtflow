97222c54765ea71450f334feabab08167c7f43c2
mpkocher <michael.kocher@me.com>
2017-09-13 18:42:59 -0700

    Write runner into EngineMultiJob Manager Actor

--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/actors/EngineMultiJobManagerActor.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/actors/EngineMultiJobManagerActor.scala
@@ -1,26 +1,18 @@
 package com.pacbio.secondary.smrtlink.actors
 
 
-import java.io.{PrintWriter, StringWriter}
-
-import akka.pattern._
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import akka.util.Timeout
 import com.pacbio.common.models.CommonModelImplicits
+import com.pacbio.common.models.CommonModels.IdAble
 import com.pacbio.secondary.smrtlink.actors.CommonMessages._
-import com.pacbio.secondary.smrtlink.analysis.jobs.JobModels.{EngineJob, EngineManagerStatus, NoAvailableWorkError}
 import com.pacbio.secondary.smrtlink.analysis.jobs.{AnalysisJobStates, JobResourceResolver}
 import com.pacbio.secondary.smrtlink.app.SmrtLinkConfigProvider
 import com.pacbio.secondary.smrtlink.dependency.Singleton
-import com.pacbio.secondary.smrtlink.jobtypes.ServiceJobRunner
+import com.pacbio.secondary.smrtlink.jobtypes.ServiceMultiJobRunner
 import com.pacbio.secondary.smrtlink.models.ConfigModels.SystemJobConfig
 
-import scala.collection.mutable
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Await, Future}
-import scala.util.control.NonFatal
-import scala.util.{Failure, Success, Try}
 
 /**
   * Created by mkocher on 9/11/17.
@@ -28,6 +20,8 @@ import scala.util.{Failure, Success, Try}
 class EngineMultiJobManagerActor(dao: JobsDao, resolver: JobResourceResolver, config:SystemJobConfig) extends Actor with ActorLogging{
 
   import CommonModelImplicits._
+
+  val runner = new ServiceMultiJobRunner(dao, config)
 
   //MK Probably want to have better model for this
   val checkForWorkInterval = 5.seconds
@@ -44,14 +38,23 @@ class EngineMultiJobManagerActor(dao: JobsDao, resolver: JobResourceResolver, co
     log.error(s"$self (pre-restart) Unhandled exception ${reason.getMessage} Message $message")
   }
 
+  def checkForWorkById(ix: IdAble) = {
+    dao.getMultiJobById(ix)
+        .map(engineJob => runner.runWorkflow(engineJob))
+        .onFailure { case ex => log.error(s"Failed to check for Multi-Job ${ix.toIdString} ${ex.getMessage}") }
+  }
+
   def checkForWork(): Unit = {
-    dao.getEngineMultiJobs(includeInactive = true).onSuccess {case jobs: Seq[EngineJob] => println(s"Found ${jobs.length} multi-jobs")}
+    dao.getEngineMultiJobs(includeInactive = true)
+        .map(jobs => jobs.filter(j => AnalysisJobStates.notCompleted(j.state)))
+        .map(jobs => jobs.map(_.id))
+        .map(ids => ids.map(checkForWorkById(_)))
   }
 
   override def receive: Receive = {
     case CheckForRunnableJob =>
-      log.info(s"$self Checking for MultiJob Work")
-      checkForWork
+      //log.info(s"$self Checking for MultiJob Work")
+      checkForWork()
 
     case x =>
       log.warning(s"Unsupported message $x to $self")
