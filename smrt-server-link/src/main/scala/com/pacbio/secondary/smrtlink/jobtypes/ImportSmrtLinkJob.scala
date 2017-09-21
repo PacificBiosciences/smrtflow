69ddc60c147fc2730eced92e7b6d5d3555a130d8
natechols <nathaniel.echols@gmail.com>
2017-09-20 20:59:54 -0700

    code cleanup

--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/jobtypes/ImportSmrtLinkJob.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/jobtypes/ImportSmrtLinkJob.scala
@@ -1,20 +1,16 @@
 package com.pacbio.secondary.smrtlink.jobtypes
 
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.{Path, Paths}
 import java.util.UUID
 
-import scala.collection.mutable.ArrayBuffer
 import scala.concurrent._
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.control.NonFatal
-import scala.util.{Try, Failure, Success}
+import scala.util.Try
 
 import org.joda.time.{DateTime => JodaDateTime}
-import org.joda.time.format.DateTimeFormat
 
-import com.pacbio.common.models.CommonModels.IdAble
-import com.pacbio.common.models.CommonModelImplicits
 import com.pacbio.secondary.smrtlink.actors.JobsDao
 import com.pacbio.secondary.smrtlink.actors.CommonMessages.MessageResponse
 import com.pacbio.secondary.smrtlink.analysis.constants.FileTypes
@@ -27,7 +23,6 @@ import com.pacbio.secondary.smrtlink.analysis.jobs.{
   AnalysisJobStates
 }
 import com.pacbio.secondary.smrtlink.analysis.jobtypes.MockJobUtils
-import com.pacbio.secondary.smrtlink.analysis.pbsmrtpipe.PbsmrtpipeConstants
 import com.pacbio.secondary.smrtlink.analysis.tools.timeUtils
 import com.pacbio.secondary.smrtlink.models.{
   DataSetMetaDataSet,
@@ -69,12 +64,69 @@ class ImportSmrtLinkJob(opts: ImportSmrtLinkJobOptions)
 
   import com.pacbio.common.models.CommonModelImplicits._
 
-  private def resolveJobs(dao: JobsDao,
-                          jobIds: Seq[IdAble]): Future[Seq[EngineJob]] =
-    Future.sequence(jobIds.map(dao.getJobById(_)))
-
   private def getUuid(id: UUID) =
     if (opts.mockJobId.getOrElse(false)) UUID.randomUUID() else id
+
+  private def getEntryPointDataStoreFiles(
+      jobId: UUID,
+      importPath: Path,
+      entryPoints: Seq[BoundEntryPoint]): Seq[DataStoreJobFile] = {
+    entryPoints.map { e =>
+      e.copy(path = importPath.resolve(e.path).toString)
+    } map { e =>
+      val now = JodaDateTime.now()
+      val md = getDataSetMiniMeta(Paths.get(e.path))
+      val f = DataStoreFile(
+        uniqueId = md.uuid, // XXX does this need to be mockable too?
+        sourceId = "import-job",
+        fileTypeId = md.metatype.fileType.fileTypeId,
+        fileSize = 0L,
+        createdAt = now,
+        modifiedAt = now,
+        path = e.path,
+        name = s"Entry point ${e.entryId}",
+        description = s"Imported entry point ${e.entryId}"
+      )
+      DataStoreJobFile(jobId, f)
+    }
+  }
+
+  private def getUniqueDataStoreFiles(
+      dataSets: Seq[Option[DataSetMetaDataSet]],
+      entryPointFiles: Seq[DataStoreJobFile]): Seq[DataStoreJobFile] = {
+    val epDsFilesUnique =
+      entryPointFiles
+        .zip(dataSets)
+        .map {
+          case (f, d) => if (d.isEmpty) Some(f) else None
+        }
+        .flatten
+    val nPresent = entryPointFiles.size - epDsFilesUnique.size
+    logger.info(s"Filtered $nPresent entry points already present in database")
+    epDsFilesUnique
+  }
+
+  private def addImportedJobFiles(
+      dao: JobsDao,
+      importedJob: EngineJob,
+      datastore: Option[PacBioDataStore]) = {
+    datastore
+      .map { ds =>
+        ds.files.map { f =>
+          // this also may have the UUID mocked for testing
+          DataStoreJobFile(
+            importedJob.uuid,
+            f.copy(path = Paths.get(importedJob.path)
+                               .resolve(f.path.toString)
+                               .toString,
+                   uniqueId = getUuid(f.uniqueId)))
+        }
+      }
+      .getOrElse(Seq.empty[DataStoreJobFile])
+      .map { f =>
+        dao.addDataStoreFile(f)
+      }
+  }
 
   override def run(
       resources: JobResourceBase,
@@ -93,58 +145,20 @@ class ImportSmrtLinkJob(opts: ImportSmrtLinkJobOptions)
     val (job, imported) = Await.result(fx1, 30.seconds)
     val importPath = Paths.get(imported.path)
     val summary = expandJob(opts.zipPath, importPath)
+    val epDsFiles = getEntryPointDataStoreFiles(job.uuid, importPath,
+                                                manifest.entryPoints)
 
-    val epDsFiles: Seq[DataStoreJobFile] = manifest.entryPoints.map { e =>
-      e.copy(path = importPath.resolve(e.path).toString)
-    } map { e =>
-      val now = JodaDateTime.now()
-      val md = getDataSetMiniMeta(Paths.get(e.path))
-      val f = DataStoreFile(
-        uniqueId = md.uuid, // XXX does this need to be mockable too?
-        sourceId = "import-job",
-        fileTypeId = md.metatype.fileType.fileTypeId,
-        fileSize = 0L,
-        createdAt = now,
-        modifiedAt = now,
-        path = e.path,
-        name = s"Entry point ${e.entryId}",
-        description = s"Imported entry point ${e.entryId}"
-      )
-      DataStoreJobFile(job.uuid, f)
-    }
     val epExisting: Seq[Option[DataSetMetaDataSet]] = epDsFiles.map { f =>
       Try {
         Await.result(dao.getDataSetMetaData(f.dataStoreFile.uniqueId),
                      30.seconds)
       }.toOption
     }
-    val epDsFilesUnique: Seq[DataStoreJobFile] =
-      epDsFiles
-        .zip(epExisting)
-        .map {
-          case (f, d) => if (d.isEmpty) Some(f) else None
-        }
-        .flatten
-    val nPresent = epDsFiles.size - epDsFilesUnique.size
-    logger.info(s"Filtered $nPresent entry points already present in database")
+    val epDsFilesUnique = getUniqueDataStoreFiles(epExisting, epDsFiles)
 
     val fx2 = for {
-      // add datastore files for imported job
       _ <- Future.sequence {
-        manifest.datastore
-          .map { ds =>
-            ds.files.map { f =>
-              // this also may have the UUID mocked for testing
-              DataStoreJobFile(
-                imported.uuid,
-                f.copy(path = importPath.resolve(f.path.toString).toString,
-                       uniqueId = getUuid(f.uniqueId)))
-            }
-          }
-          .getOrElse(Seq.empty[DataStoreJobFile])
-          .map { f =>
-            dao.addDataStoreFile(f)
-          }
+        addImportedJobFiles(dao, imported, manifest.datastore)
       }
       // import entry points as part of the import-job job, but only if they
       // are not already in the database
