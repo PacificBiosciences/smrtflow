5e39a0ac471d9acc8cf5bb888f10e41d1af1dbf4
mpkocher <michael.kocher@me.com>
2017-12-13 19:29:58 -0800

    Fix Client Base

--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/client/ClientBase.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/client/ClientBase.scala
@@ -6,11 +6,13 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.client.RequestBuilding._
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import com.pacbio.secondary.smrtlink.models.ServiceStatus
 
 /**
@@ -24,6 +26,11 @@ trait ClientBase extends Retrying {
   import SprayJsonSupport._
 
   implicit val actorSystem: ActorSystem
+  implicit val materializer = ActorMaterializer()
+  implicit val ec = actorSystem.dispatcher
+
+  lazy val http = Http()
+
   val baseUrl: URL
 
   // This should really return a URL instance, not a string
@@ -33,9 +40,6 @@ trait ClientBase extends Retrying {
             baseUrl.getPort,
             baseUrl.getPath + segment).toString
 
-  protected def serviceStatusPipeline: HttpRequest => Future[ServiceStatus] =
-    sendReceive ~> unmarshal[ServiceStatus]
-
   val statusUrl = toUrl("/status")
 
   /**
@@ -44,9 +48,9 @@ trait ClientBase extends Retrying {
     *
     * @return
     */
-  def getStatus: Future[ServiceStatus] = serviceStatusPipeline {
-    Get(statusUrl)
-  }
+  def getStatus(): Future[ServiceStatus] =
+    http.singleRequest(Get(statusUrl)).flatMap(Unmarshal(_).to[ServiceStatus])
+
   def getStatusWithRetry(
       maxRetries: Int = 3,
       retryDelay: FiniteDuration = 1.second): Future[ServiceStatus] =
