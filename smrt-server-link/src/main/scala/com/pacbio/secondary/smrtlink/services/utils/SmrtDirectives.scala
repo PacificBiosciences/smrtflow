d8f0081463211a21968ab909858d049b5e653c3e
mpkocher <michael.kocher@me.com>
2017-12-29 10:36:30 -0800

    Improve Directives for UserRecord/Jwt

--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/services/utils/SmrtDirectives.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/services/utils/SmrtDirectives.scala
@@ -1,18 +1,42 @@
 package com.pacbio.secondary.smrtlink.services.utils
 
-import akka.http.scaladsl.server.Directive
+import akka.http.scaladsl.model.HttpHeader
+import akka.http.scaladsl.model.headers.HttpChallenge
+import akka.http.scaladsl.server.AuthenticationFailedRejection.CredentialsMissing
+import akka.http.scaladsl.server.{
+  AuthenticationFailedRejection,
+  Directive,
+  ValidationRejection
+}
+import com.pacbio.secondary.smrtlink.auth.JwtUtilsImpl
 import com.pacbio.secondary.smrtlink.models.UserRecord
 
 object SmrtDirectives {
 
+  private val JWT_HEADER = "x-jwt-assertion"
+  // This should really be simplified, We don't need all these unnecessary abstractions
+  private val jwtUtils = new JwtUtilsImpl
+
+  private def parseFromHeaders(headers: Seq[HttpHeader]): Option[UserRecord] =
+    headers
+      .find(_.is(JWT_HEADER))
+      .map(_.value())
+      .flatMap(jwtUtils.parse)
+
   def extractOptionalUserRecord: Directive[Tuple1[Option[UserRecord]]] =
     Directive[Tuple1[Option[UserRecord]]] { inner => ctx =>
-      inner(Tuple1(None))(ctx)
+      inner(Tuple1(parseFromHeaders(ctx.request.headers)))(ctx)
     }
 
   def extractRequiredUserRecord: Directive[Tuple1[UserRecord]] =
     Directive[Tuple1[UserRecord]] { inner => ctx =>
-      inner(Tuple1(UserRecord("stuff", Some("more-stuff"))))(ctx)
+      parseFromHeaders(ctx.request.headers) match {
+        case Some(userRecord) => inner(Tuple1(userRecord))(ctx)
+        case _ =>
+          ctx.reject(
+            AuthenticationFailedRejection(CredentialsMissing,
+                                          HttpChallenge("http", realm = None)))
+      }
     }
 
 }
