b751371845fc1c4685298758c0bd848f7f855aea
mcantor <mcantor@pacificbiosciences.com>
2018-02-26 12:07:08 -0800

    [SL-2524] in migration script for adding chip type, uses a second SET statement rather than "WITH VALUES" to make all entries 1mChip.

--- smrt-server-link/src/main/scala/db/migration/V14__AddChipTypeToRun.scala
+++ smrt-server-link/src/main/scala/db/migration/V14__AddChipTypeToRun.scala
@@ -15,7 +15,8 @@ class V14__AddChipTypeToRun
   override def slickMigrate(db: DatabaseDef): Future[Any] = {
     db.run(
       DBIO.seq(
-        sqlu"""ALTER TABLE run_summaries ADD COLUMN chip_type VARCHAR(256) NOT NULL DEFAULT '1mChip' WITH VALUES"""
+        sqlu"""ALTER TABLE run_summaries ADD COLUMN chip_type VARCHAR(256) NOT NULL DEFAULT '1mChip'""",
+        sqlu"""UPDATE run_summaries SET chip_type = '1mChip'"""
       ))
   }
 }
