6cf39c66d1b2ad52389974e34eb6198c7ef69030
mpkocher <michael.kocher@me.com>
2018-03-01 10:19:11 -0800

    Send GZIP header.

--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/client/ClientBase.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/client/ClientBase.scala
@@ -4,19 +4,16 @@ import java.net.URL
 
 import akka.actor.{ActorSystem, Scheduler}
 import akka.http.scaladsl.client.RequestBuilding._
-import akka.http.scaladsl.model.{
-  HttpRequest,
-  HttpResponse,
-  StatusCode,
-  StatusCodes
-}
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.unmarshalling.{Unmarshal, Unmarshaller}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.coding.{Deflate, Gzip, NoCoding}
+import akka.http.scaladsl.model.headers.{HttpEncodings, `Accept-Encoding`}
 import akka.pattern.after
 import akka.stream.ActorMaterializer
 
-import scala.concurrent.{Future, Await, ExecutionContext}
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration._
 //import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.control.NonFatal
@@ -75,23 +72,58 @@ trait ClientBase extends Retrying {
   protected def getEndpoint(endpointUrl: String): Future[HttpResponse] =
     http.singleRequest(Get(endpointUrl))
 
-  protected def getResponse(request: HttpRequest): Future[HttpResponse] =
-    http
-      .singleRequest(request)
-      .flatMap { response =>
-        response.status match {
-          case StatusCodes.OK | StatusCodes.Created =>
-            Future.successful(response)
-          case _ =>
-            Future.failed(
-              new Exception(
-                s"HTTP ERROR ${response.status}: ${response.toString}"))
-        }
+  private def decodeResponse(response: HttpResponse): HttpResponse = {
+    val decoder = response.encoding match {
+      case HttpEncodings.gzip ⇒
+        Gzip
+      case HttpEncodings.deflate ⇒
+        Deflate
+      case HttpEncodings.identity ⇒
+        NoCoding
+      case _ =>
+        // This perhaps should raise?
+        NoCoding
+    }
+
+    decoder.decodeMessage(response)
+  }
+
+  // Useful for debugging
+  private val customLogRequest: HttpRequest => HttpRequest = { r =>
+    println(r.toString)
+    r
+  }
+
+  private val customLogResponse: HttpResponse => HttpResponse = { r =>
+    //println(r.toString)
+    println(s"Status         ${r.status}")
+    println(s"Headers        ${r.headers}")
+    println(s"Content Length ${r.entity.contentLengthOption}")
+    r
+  }
+
+  protected def getResponse(request: HttpRequest): Future[HttpResponse] = {
+
+    val req = request.addHeader(
+      `Accept-Encoding`(HttpEncodings.gzip, HttpEncodings.deflate))
+
+
+    http.singleRequest(req).flatMap { response =>
+      response.status match {
+        case StatusCodes.OK | StatusCodes.Created =>
+          customLogResponse(response)
+          Future.successful(response)
+        case _ =>
+          Future.failed(
+            new Exception(
+              s"HTTP ERROR ${response.status}: ${response.toString}"))
       }
+    }
+  }
 
   protected def getObject[T](request: HttpRequest)(
       implicit um: Unmarshaller[HttpResponse, T]): Future[T] =
-    getResponse(request).flatMap(um(_))
+    getResponse(request).map(decodeResponse).flatMap(um(_))
 
   /**
     * Get Status of the System. The model must adhere to the SmrtServer Status
