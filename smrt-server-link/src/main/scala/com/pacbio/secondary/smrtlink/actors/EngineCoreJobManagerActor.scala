5f8507637be763b13d81371acf582f05c2785bc5
mpkocher <michael.kocher@me.com>
2018-04-23 18:20:20 -0700

    Mark a Run as non-editable when states changes away from Ready.
    This will mark the MultiJob as SUBMITTED.

--- smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/actors/EngineCoreJobManagerActor.scala
+++ smrt-server-link/src/main/scala/com/pacbio/secondary/smrtlink/actors/EngineCoreJobManagerActor.scala
@@ -378,7 +378,7 @@ class EngineCoreJobManagerActor(dao: JobsDao,
       * will never be imported. In this case, update the Child Job state to FAILED with a specific error message.
       * */
     case RunChangedStateMessage(runSummary) =>
-      if (runSummary.reserved) {
+      if (runSummary.reserved && (runSummary.status != SupportedRunStates.READY)) {
         runSummary.multiJobId match {
           case Some(multiJobId) =>
             logResultsMessage(
