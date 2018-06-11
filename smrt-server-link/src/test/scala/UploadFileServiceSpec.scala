8d749ad456a9fc61ad4afe8a38b977125de0ec98
mpkocher <michael.kocher@me.com>
2018-06-11 16:37:07 -0700

    Simplify self trait in UploadFileService spec.

--- smrt-server-link/src/test/scala/UploadFileServiceSpec.scala
+++ smrt-server-link/src/test/scala/UploadFileServiceSpec.scala
@@ -2,16 +2,27 @@ import java.io.File
 
 import org.specs2.mutable.Specification
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.actor.ActorRefFactory
+import akka.actor.{ActorRefFactory, ActorSystem}
 import akka.http.scaladsl.model.{ContentTypes, Multipart, StatusCodes}
 import akka.http.scaladsl.testkit.Specs2RouteTest
 import com.pacbio.secondary.smrtlink.auth._
-import com.pacbio.secondary.smrtlink.dependency.{SetBindings, Singleton}
+import com.pacbio.secondary.smrtlink.dependency.{
+  ConfigProvider,
+  SetBindings,
+  Singleton
+}
 import com.pacbio.secondary.smrtlink.services.PacBioServiceErrors
 import com.pacbio.secondary.smrtlink.time.FakeClockProvider
-import com.pacbio.secondary.smrtlink.analysis.configloaders.{EngineCoreConfigLoader, PbsmrtpipeConfigLoader}
+import com.pacbio.secondary.smrtlink.analysis.configloaders.{
+  EngineCoreConfigLoader,
+  PbsmrtpipeConfigLoader
+}
 import com.pacbio.secondary.smrtlink.{JobServiceConstants, SmrtLinkConstants}
-import com.pacbio.secondary.smrtlink.actors.{ActorRefFactoryProvider, SmrtLinkDalProvider, _}
+import com.pacbio.secondary.smrtlink.actors.{
+  ActorRefFactoryProvider,
+  SmrtLinkDalProvider,
+  _
+}
 import com.pacbio.secondary.smrtlink.app.SmrtLinkConfigProvider
 import com.pacbio.secondary.smrtlink.services._
 import com.pacbio.secondary.smrtlink.models._
@@ -22,6 +33,7 @@ import java.nio.file.{Files, Path}
 
 import akka.http.scaladsl.model.headers.RawHeader
 import com.pacbio.secondary.smrtlink.auth.JwtUtils.JWT_HEADER
+import com.typesafe.config.Config
 
 /**
   * This spec has been updated to support multiple runs (i.e.,
@@ -29,7 +41,7 @@ import com.pacbio.secondary.smrtlink.auth.JwtUtils.JWT_HEADER
   *
   */
 class UploadFileServiceSpec
-  extends Specification
+    extends Specification
     with Specs2RouteTest
     with SetupMockData
     with PacBioServiceErrors
@@ -46,57 +58,32 @@ class UploadFileServiceSpec
   val READ_CREDENTIALS = RawHeader(JWT_HEADER, READ_USER_LOGIN)
   val testSmrtLinkVersion = "1.2.3"
 
-  trait TestUploadFileServiceProvider {
-    this: ActorSystemProvider
-      with SmrtLinkTestDalProvider
-      with SmrtLinkConfigProvider
-      with ServiceComposer =>
-
-    val uploadFileService: Singleton[UploadFileService] =
-      Singleton { () =>
-        new UploadFileService(smrtLinkTempDir())(system)
-      }
-
-    addService(uploadFileService)
-  }
-
-  //  trait UploadFileServiceProvider {
-  //    this: ActorSystemProvider with SmrtLinkConfigProvider with ServiceComposer =>
-  //
-  //    val uploadFileService: Singleton[UploadFileService] =
-  //      Singleton { () =>
-  //        implicit val system = actorSystem()
-  //        new UploadFileService(smrtLinkTempDir())(system)
-  //      }
-  //
-  //    addService(uploadFileService)
-  //  }
-
   object TestProviders
-    extends ServiceComposer
+      extends ServiceComposer
+      with SmrtLinkConfigProvider
       with ProjectServiceProvider
       with SmrtLinkTestDalProvider
-      with SmrtLinkConfigProvider
+      with ConfigProvider
+      with ActorSystemProvider
       with PbsmrtpipeConfigLoader
       with EngineCoreConfigLoader
-      with TestUploadFileServiceProvider
+      with UploadFileServiceProvider
       with DataSetServiceProvider
       with EventManagerActorProvider
       with JobsDaoProvider
       with JwtUtilsProvider
       with FakeClockProvider
-      with SetBindings
-      with ActorRefFactoryProvider {
+      with SetBindings {
 
-    // Provide a fake JwtUtils that uses the login as the JWT, and validates every JWT except for invalidJwt.
     override final val jwtUtils: Singleton[JwtUtils] = Singleton(() =>
       new JwtUtils {
         override def parse(jwt: String): Option[UserRecord] =
           if (jwt == INVALID_JWT) None else Some(UserRecord(jwt))
-      })
+    })
 
-    override val actorRefFactory: Singleton[ActorRefFactory] = Singleton(
-      system)
+    override val config: Singleton[Config] = Singleton(testConfig)
+    override val actorSystem: Singleton[ActorSystem] = Singleton(system)
+    override val actorRefFactory: Singleton[ActorRefFactory] = actorSystem
   }
 
   override val dao: JobsDao = TestProviders.jobsDao()
@@ -104,21 +91,21 @@ class UploadFileServiceSpec
   val totalRoutes = TestProviders.uploadFileService().prefixedRoutes
   step(setupDb(TestProviders.dbConfig))
 
-  case class UploadResponse(path: String);
+  case class UploadResponse(path: String)
 
   "Upload File service" should {
     "upload a fasta file" in {
       val fastaPath: Path = MockFileUtils.writeMockTmpFastaFile()
-      val fileData = Multipart.FormData.BodyPart.fromPath("mockFasta", ContentTypes.`text/plain(UTF-8)`, fastaPath)
+      val fileData = Multipart.FormData.BodyPart
+        .fromPath("mockFasta", ContentTypes.`text/plain(UTF-8)`, fastaPath)
       val formData = Multipart.FormData(fileData)
-      Post("/smrt-link/uploader", formData)  ~> addHeader(READ_CREDENTIALS) ~> totalRoutes ~> check {
+      Post("/smrt-link/uploader", formData) ~> addHeader(READ_CREDENTIALS) ~> totalRoutes ~> check {
         status shouldEqual StatusCodes.OK
-        val response = responseAs[UploadResponse];
-        val file = new File(response.path);
+        val response = responseAs[UploadResponse]
+        val file = new File(response.path)
         file.exists must beTrue
       }
     }
   }
 
 }
-
