a01b4ffe0ac6e2f33ac9e7f068f378186c64dcba
natechols <nathaniel.echols@gmail.com>
2018-06-29 18:30:19 -0700

    apply migration to inactive datasets

--- smrt-server-link/src/main/scala/db/migration/V22__AddDataSetReportsTable.scala
+++ smrt-server-link/src/main/scala/db/migration/V22__AddDataSetReportsTable.scala
@@ -22,7 +22,7 @@ class V22__AddDataSetReportsTable
     sqlu"""create table "dataset_reports" ("dataset_uuid" UUID NOT NULL,"report_uuid" UUID NOT NULL); create index "index_dataset_uuid" on "dataset_reports" ("dataset_uuid"); create index "index_report_uuid" on "dataset_reports" ("report_uuid"); alter table "dataset_reports" add constraint "rpt_fk" foreign key("report_uuid") references "datastore_files"("uuid") on update NO ACTION on delete NO ACTION"""
 
   def qInsertDataSetReports: DBIO[Int] =
-    sqlu"INSERT INTO dataset_reports (dataset_uuid, report_uuid) SELECT dataset_metadata.uuid, datastore_files.uuid FROM dataset_metadata JOIN datastore_files ON (datastore_files.job_id = dataset_metadata.job_id AND datastore_files.file_type_id = 'PacBio.FileTypes.JsonReport' AND dataset_metadata.is_active = true)";
+    sqlu"INSERT INTO dataset_reports (dataset_uuid, report_uuid) SELECT dataset_metadata.uuid, datastore_files.uuid FROM dataset_metadata JOIN datastore_files ON (datastore_files.job_id = dataset_metadata.job_id AND datastore_files.file_type_id = 'PacBio.FileTypes.JsonReport')";
 
   override def slickMigrate(db: DatabaseDef): Future[Any] =
     db.run(
